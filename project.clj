e15737d4306d8bfcab4c3ac48eb6c2c5ac4d3691
Jens Bendisposto <jens@bendisposto.de>
2015-11-09 14:15:47 +0100

    initial import

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject lisb "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["snapshot" "https://oss.sonatype.org/content/repositories/snapshots"]]
+  :dependencies [[org.clojure/clojure "1.8.0-beta2"]
+                 [de.hhu.stups/de.prob2.kernel,"2.0.0-SNAPSHOT"]])
---
+++ src/lisb/core.clj
@@ -0,0 +1,56 @@
+(ns lisb.core
+  (:import de.prob.Main
+           de.prob.scripting.Api
+           de.prob.animator.command.CbcSolveCommand
+           de.prob.animator.domainobjects.ClassicalB
+           (de.be4.classicalb.core.parser.node Start
+                                               EOF
+                                               APredicateParseUnit
+                                               AIntegerExpression
+                                               AIdentifierExpression
+                                               TIntegerLiteral
+                                               TIdentifierLiteral
+                                               ALessPredicate)))
+
+
+(defn get-api [] (.getInstance (Main/getInjector) Api))
+
+(defn create-empty-machine []
+  (let [tf (java.io.File/createTempFile "evalb" ".mch" nil)
+        tn (.getAbsolutePath tf)
+        ]
+    (.deleteOnExit tf)
+    (spit tf "MACHINE empty \n END")
+    tn))
+
+(defn state-space []
+  (let [machine (create-empty-machine)
+        api (get-api)]
+    (.b_load api machine)))
+
+
+(defn predicate [ast]
+  (let [p (APredicateParseUnit. ast)
+        start (Start. p (EOF.))]
+    (ClassicalB. start)))
+
+(defn identifier [n]
+  (let [token (TIdentifierLiteral. n)]
+    (AIdentifierExpression. [token])))
+
+(defn integer [n]
+  (let [token (TIntegerLiteral. (str n))]
+    (AIntegerExpression. token)))
+
+(defn less [l r]
+  (ALessPredicate. l r))
+
+(defn eval [state-space ast]
+  (let [cmd (CbcSolveCommand. (predicate ast))
+        _ (.execute state-space cmd)
+        free (.getFreeVariables cmd)
+        result (.. cmd getValue translate)
+        ]
+    (when (.. result getValue booleanValue)
+      (into {} (map (fn [k][k (.getSolution result k)]) free)))))
+
