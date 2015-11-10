4611ebe659af08451a8158841b55e87557a55b64
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2015-11-10 14:51:02 +0100

    add test file

---
+++ test/lisb/core_test.clj
@@ -0,0 +1,40 @@
+(ns lisb.core-test
+  (:require [clojure.test :refer :all]
+            [lisb.core :refer :all]))
+
+(deftest conjunct-test
+  (testing "conjunction of two values works"
+    (is (= {:tag :and
+            :children [:a :b]}
+           (conjunct :a :b)))))
+
+
+(deftest chaining-test
+  (testing "chaining generates conjunctions"
+    (is (= {:tag :and
+            :children [[:a :b] [:b :c]]}
+           (chain vector [:a :b :c])))))
+
+(deftest less-test
+  (testing "less works with two arguments"
+    (is (= {:tag :less
+            :children [:a :b]}
+           (b< :a :b))))
+  (testing "less works with more than two arguments"
+    (is (= {:tag :and
+            :children [{:tag :less :children [:a :b]}
+                       {:tag :less :children [:b :c]}]}
+           (b< :a :b :c)))))
+
+(deftest plus-test
+  (testing "plus works with two arguments"
+    (is (= {:tag :plus
+            :children [:a :b]}
+           (b+ :a :b))))
+  (testing "plus works with more than two arguments"
+    (is (= {:tag :plus
+            :children [{:tag :plus :children [:a :b]}
+                       :c]}
+           (b+ :a :b :c)))))
+
+
