8c33b4d7a10a47a617731db1bd69a3a6a1c94b98
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2015-11-10 21:34:08 +0100

    another refactoring

--- src/lisb/representation.clj
+++ src/lisb/representation.clj
@@ -5,27 +5,30 @@
   {:tag tag
    :children children})
 
+
 (defn chain-arity-two
   [tag nodes]
   (let [tuples (partition 2 1 nodes)]
     (reduce (partial node :and) (map (partial apply node tag) tuples))))
 
+(defn interleave-arity-two [tag nodes]
+  (reduce (partial node tag) nodes))
+
+
 
 (defn b< [& args]
   (chain-arity-two :less args))
 
-
 (defn b+ [& args]
-  (reduce (partial node :plus) args))
-
+  (interleave-arity-two :plus args))
 
 (defn b- [a & r]
   (if (seq r)
-    (reduce (partial node :minus) a r)
+    (interleave-arity-two :minus (conj r a))
     (node :unaryminus a)))
 
 (defn band [& args]
-  (reduce (partial node :and) args))
+  (interleave-arity-two :and args))
 
 (defn b= [& args]
   (chain-arity-two :equals args))
