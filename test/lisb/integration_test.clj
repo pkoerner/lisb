b38fb11ab6fe451b4dc19406a7436cdffd16364c
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2015-11-11 11:23:27 +0100

    improve tests

--- test/lisb/integration_test.clj
+++ test/lisb/integration_test.clj
@@ -11,33 +11,33 @@
             for now only 'truish' predicates are evaluated because eval cannot handle
             ComputationNotCompletedResults yet"
     (let [ss (state-space)]
-      (apply eval ss (to-ast (b= 1 1)))
-      (apply eval ss (to-ast (b= :x 1)))
-      (apply eval ss (to-ast (b= 1 1 1)))
-      (apply eval ss (to-ast (b= :x true)))
-      (apply eval ss (to-ast (b= :x false)))
+      (is (apply eval ss (to-ast (b= 1 1))))
+      (is (apply eval ss (to-ast (b= :x 1))))
+      (is (apply eval ss (to-ast (b= 1 1 1))))
+      (is (apply eval ss (to-ast (b= :x true))))
+      (is (apply eval ss (to-ast (b= :x false))))
 
-      (apply eval ss (to-ast (bnot= :x 1)))
-      (apply eval ss (to-ast (bnot= :x 1 2)))
+      (is (apply eval ss (to-ast (bnot= :x 1))))
+      (is (apply eval ss (to-ast (bnot= :x 1 2))))
 
-      (apply eval ss (to-ast (b< 1 2)))
-      (apply eval ss (to-ast (b< 1 2 3)))
+      (is (apply eval ss (to-ast (b< 1 2))))
+      (is (apply eval ss (to-ast (b< 1 2 3))))
 
-      (apply eval ss (to-ast (b< 1 (b+ 1 2))))
-      (apply eval ss (to-ast (b< 1 (b+ 1 2 3))))
+      (is (apply eval ss (to-ast (b< 1 (b+ 1 2)))))
+      (is (apply eval ss (to-ast (b< 1 (b+ 1 2 3)))))
 
-      (apply eval ss (to-ast (b< 1 (b- 10 2))))
-      (apply eval ss (to-ast (b< 1 (b- 10 2 3))))
+      (is (apply eval ss (to-ast (b< 1 (b- 10 2)))))
+      (is (apply eval ss (to-ast (b< 1 (b- 10 2 3)))))
 
-      (apply eval ss (to-ast (band (b< 1 2) (b< 2 3))))
-      (apply eval ss (to-ast (band (b< 1 2) (b< 2 3) (b< 3 4))))
+      (is (apply eval ss (to-ast (band (b< 1 2) (b< 2 3)))))
+      (is (apply eval ss (to-ast (band (b< 1 2) (b< 2 3) (b< 3 4)))))
 
-      (apply eval ss (to-ast (bor (b< 1 2) (b< 2 3))))
-      (apply eval ss (to-ast (bor (b< 1 2) (b< 2 3) (b< 3 4))))
+      (is (apply eval ss (to-ast (bor (b< 1 2) (b< 2 3)))))
+      (is (apply eval ss (to-ast (bor (b< 1 2) (b< 2 3) (b< 3 4)))))
 
-      (apply eval ss (to-ast (b<=> (b< 1 2) (b< 2 3))))
-      (apply eval ss (to-ast (b<=> (b< 1 2) (b< 2 3) (b< 3 4))))
+      (is (apply eval ss (to-ast (b<=> (b< 1 2) (b< 2 3)))))
+      (is (apply eval ss (to-ast (b<=> (b< 1 2) (b< 2 3) (b< 3 4)))))
 
-      (apply eval ss (to-ast (bnot (b< 2 1))))
+      (is (apply eval ss (to-ast (bnot (b< 2 1)))))
       )))
 
