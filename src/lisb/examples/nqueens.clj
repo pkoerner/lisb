0469d175c64f29e002663849c2ec074910336c4d
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2015-11-24 17:08:39 +0100

    paste an n-queens solution

--- src/lisb/examples/nqueens.clj
+++ src/lisb/examples/nqueens.clj
@@ -68,3 +68,28 @@
    (defonce ss (state-space))
    (nqueens3 size ss)))
 
+
+
+
+
+
+
+;;; a solution in Clojure
+;;; stolen from https://github.com/jeffbrown/clojure-n-queens.git
+
+(defn is-valid-addition?
+    [board proposed-position]
+    (let [board-size (count board)]
+        (not-any? true?
+            (for [row-counter (range board-size) :let [pos (board row-counter)]]
+                (or
+                    (= pos proposed-position)
+                    (= (- board-size row-counter) (Math/abs (- proposed-position pos))))))))
+
+(defn add-to-boards
+    [boards board-size]
+    (for [board boards column-counter (range 1 (inc board-size)) :when (is-valid-addition? board column-counter)]
+          (conj board column-counter)))
+          
+(defn solve-for-board-size [board-size]
+    (nth (iterate (fn [boards] (add-to-boards boards board-size)) [[]]) board-size))
