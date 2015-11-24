3fcfb61a6f2b1e4c4ac06ceba70031ae699a8bf6
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2015-11-24 16:02:34 +0100

    a solution for send more money in clojure

--- src/lisb/examples/send_more_money.clj
+++ src/lisb/examples/send_more_money.clj
@@ -17,3 +17,18 @@
   ([]
    (defonce ss (state-space))
    (send-more-money ss)))
+
+(defn send-more-money-clj []
+  (for [s (range 1 10)
+        e (remove #{s} (range 10))
+        n (remove #{s e} (range 10))
+        d (remove #{s e n} (range 10))
+        m (remove #{s e n d} (range 1 10))
+        o (remove #{s e n d m} (range 10))
+        r (remove #{s e n d m o} (range 10))
+        y (remove #{s e n d m o r} (range 10))
+        :when (= (+ (* 1000 s) (* 100 e) (* 10 n) d
+                    (* 1000 m) (* 100 o) (* 10 r) e)
+                 (+ (* 10000 m) (* 1000 o) (* 100 n) (* 10 e) y))]
+    {"s" s, "e" e, "n" n, "d" d,
+     "m" m, "o" o, "r" r, "y" y}))
