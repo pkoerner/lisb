9113898a68923283c7e8d726938cd99663bb357c
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2016-01-18 23:20:55 +0100

    some refactoring, implement more cases

--- src/lisb/AST2lisb.clj
+++ src/lisb/AST2lisb.clj
@@ -7,58 +7,68 @@
                          (.getRight node)
                          s))
 
+
+(defn translate-to-lisb [instance sb fname & args]
+  (.append sb (str "(b" (clojure.string/trim fname)))
+  (doseq [arg args]
+    (.append sb " ")
+    (.apply arg instance))
+  (.append sb ")"))
+
+
 (defn get-visitor [sb]
   (proxy [de.prob.model.classicalb.PrettyPrinter] []
     (getPrettyPrint []
       (.toString sb))
 
-    (caseAIntegerExpression [expr]
-      (.append sb (.. expr getLiteral getText)))
+    (caseAIntegerExpression [expr] (.append sb (.. expr getLiteral getText)))
+    (caseANatSetExpression      [_] (.append sb "(bnat-set)"))
+    (caseANat1SetExpression     [_] (.append sb "(bnat1-set)"))
+    (caseANaturalSetExpression  [_] (.append sb "(bnatural-set)"))
+    (caseANatural1SetExpression [_] (.append sb "(bnatural1-set)"))
+    (caseAIntSetExpression      [_] (.append sb "(bint-set)"))
+    (caseAIntegerSetExpression  [_] (.append sb "(binteger-set)"))
 
-    (caseABooleanTrueExpression [expr]
-      (.append sb " true "))
+    (caseABooleanTrueExpression  [_] (.append sb " true "))
+    (caseABooleanFalseExpression [_] (.append sb " false "))
+    (caseABoolSetExpression      [_] (.append sb "(bbool-set)"))
+    (caseAConvertBoolExpression  [node] (translate-to-lisb this sb "pred->bool" (.getPredicate node)))
 
-    (caseABooleanFalseExpression [expr]
-      (.append sb " false "))
-
-
-    (caseAEmptySetExpression [_]
-      (.append sb "#{}"))
-
+    (caseAEmptySetExpression [_] (.append sb "#{}"))
     (caseASetExtensionExpression [node]
-      (.append sb "#{")
-      (doseq [n (.getExpressions node)]
-        (.apply n this)
-        (.append sb " "))
-      (.append sb "}"))
+      (apply translate-to-lisb this sb "set-enum" (.getExpressions node)))
 
-    (caseACoupleExpression [node]
-      (.append sb "(bcouple ")
-      (.apply (first (.getList node)) this)
-      (.append sb " ")
-      (.apply (second (.getList node)) this)
-      (.append sb ")"))
+    (caseACoupleExpression [node] (apply translate-to-lisb this sb "couple" (.getList node)))
 
     (applyLeftAssociative [left node right append]
-      (.append sb "(b")
-      (.append sb (clojure.string/trim append))
-      (.append sb " ")
-      (.apply left this)
-      (.append sb " ")
-      (.apply right this)
-      (.append sb ")"))  
-
+      (translate-to-lisb this sb append left right))
     (applyRightAssociative [left node right append]
       (.applyLeftAssociative this left node right append))
 
-    (caseAConjunctPredicate [node]
-      (overwrite-appendstr this node "and"))
-    (caseADisjunctPredicate [node]
-      (overwrite-appendstr this node "or"))
-    (caseAUnionExpression [node]
-      (overwrite-appendstr this node "union"))
-    (caseAIntersectionExpression [node]
-      (overwrite-appendstr this node "intersection"))))
+    (caseADivExpression          [node] (overwrite-appendstr this node "div"))
+    (caseAMaxExpression          [node] (translate-to-lisb this sb "max" (.getExpression node)))
+    (caseAMinExpression          [node] (translate-to-lisb this sb "min" (.getExpression node)))
+
+    (caseAConjunctPredicate      [node] (overwrite-appendstr this node "and"))
+    (caseADisjunctPredicate      [node] (overwrite-appendstr this node "or"))
+    (caseANegationPredicate      [node] (translate-to-lisb this sb "not" (.getPredicate node)))
+
+    (caseASubsetPredicate        [node] (overwrite-appendstr this node "subset"))
+    (caseASubsetStrictPredicate  [node] (overwrite-appendstr this node "subset-strict"))
+    (caseAMemberPredicate        [node] (overwrite-appendstr this node "member"))
+    (caseAUnionExpression        [node] (overwrite-appendstr this node "union"))
+    (caseAIntersectionExpression [node] (overwrite-appendstr this node "intersection"))
+    (caseAPowSubsetExpression    [node] (translate-to-lisb this sb "pow" (.getExpression node)))
+    (caseAPow1SubsetExpression   [node] (translate-to-lisb this sb "pow1" (.getExpression node)))
+    (caseAFinSubsetExpression    [node] (translate-to-lisb this sb "fin" (.getExpression node)))
+    (caseAFin1SubsetExpression   [node] (translate-to-lisb this sb "fin1" (.getExpression node)))
+    (caseACardExpression         [node] (translate-to-lisb this sb "count" (.getExpression node)))
+
+    (caseADomainExpression       [node] (translate-to-lisb this sb "dom" (.getExpression node)))
+    (caseARangeExpression        [node] (translate-to-lisb this sb "ran" (.getExpression node)))
+    (caseAImageExpression        [node] (translate-to-lisb this sb "image" (.getLeft node) (.getRight node)))
+    (caseAReverseExpression      [node] (translate-to-lisb this sb "inverse" (.getExpression node)))
+    ))
 
 (defn fresh-visitor []
   (get-visitor (StringBuffer.)))
--- test/lisb/AST2lisb_test.clj
+++ test/lisb/AST2lisb_test.clj
@@ -8,17 +8,26 @@
     (is (= 1 (parse-print-read "1"))))
   (testing "booleans"
     (is (= true  (parse-print-read "TRUE")))
-    (is (= false (parse-print-read "FALSE"))))
+    (is (= false (parse-print-read "FALSE")))
+    (is (= '(bpred->bool (b= 1 1)) (parse-print-read "bool(1=1)")))) 
   (testing "sets"
     (is (= #{}    (parse-print-read "{}")))
-    (is (= #{1}   (parse-print-read "{1}"))) 
-    (is (= #{1 2} (parse-print-read "{1,2}"))))
-    (is (= '#{(bcouple 1 2)} (parse-print-read "{1|->2}"))) 
+    (is (= '(bset-enum 1)    (parse-print-read "{1}"))) 
+    (is (= '(bset-enum 1 2)  (parse-print-read "{1,2}")))
+    (is (= '(bcouple 1 2)    (parse-print-read "1|->2")))
+    (is (= '(bbool-set)      (parse-print-read "BOOL")))
+    (is (= '(bnat-set)       (parse-print-read "NAT")))
+    (is (= '(bnat1-set)      (parse-print-read "NAT1")))
+    (is (= '(bnatural-set)   (parse-print-read "NATURAL")))
+    (is (= '(bnatural1-set)  (parse-print-read "NATURAL1")))
+    (is (= '(bint-set)       (parse-print-read "INT")))
+    (is (= '(binteger-set)   (parse-print-read "INTEGER"))))
   (testing "some integer operations"
     (are [x y] (= x (parse-print-read y))
       '(b+ 1 2)    "1+2"
       '(b- 1 2)    "1-2"
       '(b* 1 2)    "1*2"
+      '(bdiv 1 2)  "1 / 2"
       '(b< 1 2)    "1<2"
       '(b> 1 2)    "1>2"
       '(b<= 1 2)   "1<=2"
@@ -26,18 +35,32 @@
       '(bmod 1 2)  "1 mod 2"
       '(b= 1 2)    "1=2"
       '(b** 1 2)   "1 ** 2"
-      '(b** 1 (b** 2 3)) "1 ** 2 ** 3"))
+      '(b** 1 (b** 2 3)) "1 ** 2 ** 3")
+      '(bmax (benum-set 1 2)) "max({1,2})"
+      '(bmin (benum-set 1 2)) "min({1,2})"
+    )
   (testing "logical predicates"
     (are [x y] (= x (parse-print-read y))
       '(band (b= 1 1) (b= 1 2))   "(1=1) & (1=2)"
       '(bor (b= 1 1) (b= 1 2))    "(1=1) or (1=2)"
       '(b<=> (b= 1 1) (b= 1 2))   "(1=1) <=> (1=2)"
-      '(b=> (b= 1 1) (b= 1 2))    "(1=1) => (1=2)"))
+      '(b=> (b= 1 1) (b= 1 2))    "(1=1) => (1=2)"
+      '(bnot (b= 1 1))            "not(1=1)"
+      ))
   (testing "sets operations"
     (are [x y] (= x (parse-print-read y))
-      '(b* #{} #{})        "{} * {}"
-      '(bunion #{1} #{2})  "{1} \\/ {2}"
-      '(bintersection #{1} #{2})  "{1} /\\ {2}"))
+      '(b* #{} #{})              "{} * {}"
+      '(bunion #{} #{})          "{} \\/ {}"
+      '(bintersection #{} #{})   "{} /\\ {}"
+      '(bmember 1 (bset-enum 1)) "1 : {1}"
+      '(bpow (bset-enum 1))      "POW({1})"
+      '(bpow1 (bset-enum 1))     "POW1({1})"
+      '(bfin (bset-enum 1))      "FIN({1})"
+      '(bfin1 (bset-enum 1))     "FIN1({1})"
+      '(bsubset #{} (bset-enum 1)) "{} <: {1}"
+      '(bsubset-strict #{} (bset-enum 1)) "{} <<: {1}"
+      '(bcount (bset-enum 1))    "card({1})"
+      ))
   (testing "relations"
     (are [x y] (= x (parse-print-read y))
       '(b<-> #{} #{})  "{} <-> {}"
@@ -46,7 +69,13 @@
       '(b|> #{} #{})   "{} |> {}"
       '(b|>> #{} #{})  "{} |>> {}"
       '(b<+ #{} #{})   "{} <+ {}"
-      '(b>< #{} #{})   "{} >< {}"))
+      '(b>< #{} #{})   "{} >< {}"
+      '(bdom #{})      "dom({})"
+      '(bran #{})      "ran({})"
+      '(bimage #{} (bset-enum 1)) "{}[{1}]"
+      '(binverse #{})      "{}~"
+      
+      ))
   (testing "functions"
     (are [x y] (= x (parse-print-read y))
       '(b+-> #{} #{})   "{} +-> {}"
