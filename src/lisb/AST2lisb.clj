78127d3b3d19faa15b8b972d8e909a49fa7addaf
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2016-01-19 12:15:32 +0100

    work on more stuff

--- src/lisb/AST2lisb.clj
+++ src/lisb/AST2lisb.clj
@@ -11,12 +11,16 @@
 (defn translate-to-lisb [instance sb fname & args]
   (.append sb (str "(b" (clojure.string/trim fname)))
   (doseq [arg args]
-    (.append sb " ")
-    (.apply arg instance))
+    (if (instance? java.util.List arg)
+      (do (.append sb "[")
+          (doseq [e arg] (.append sb " ") (.apply e instance))
+          (.append sb "]"))
+      (do (.append sb " ")
+          (.apply arg instance))))
   (.append sb ")"))
 
 
-(defn get-visitor [sb]
+(defn get-visitor [sb symbols]
   (proxy [de.prob.model.classicalb.PrettyPrinter] []
     (getPrettyPrint []
       (.toString sb))
@@ -40,19 +44,32 @@
 
     (caseACoupleExpression [node] (apply translate-to-lisb this sb "couple" (.getList node)))
 
+    (caseTIdentifierLiteral [node]
+      (let [s (.getText node)]
+        (.append sb (if (symbols s) s (str (keyword s))))))
+
     (applyLeftAssociative [left node right append]
       (translate-to-lisb this sb append left right))
     (applyRightAssociative [left node right append]
       (.applyLeftAssociative this left node right append))
 
+    (caseAUnaryMinusExpression   [node] (translate-to-lisb this sb "-" (.getExpression node)))
     (caseADivExpression          [node] (overwrite-appendstr this node "div"))
     (caseAMaxExpression          [node] (translate-to-lisb this sb "max" (.getExpression node)))
     (caseAMinExpression          [node] (translate-to-lisb this sb "min" (.getExpression node)))
+    (caseAIntervalExpression     [node] (translate-to-lisb this sb "interval" (.getLeftBorder node) (.getRightBorder node)))
+    (caseAGeneralSumExpression   [node] (translate-to-lisb this sb "sigma" (.getIdentifiers node) (.getPredicates node) (.getExpression node)))
+    (caseAGeneralProductExpression [node] (translate-to-lisb this sb "pi" (.getIdentifiers node) (.getPredicates node) (.getExpression node)))
 
     (caseAConjunctPredicate      [node] (overwrite-appendstr this node "and"))
     (caseADisjunctPredicate      [node] (overwrite-appendstr this node "or"))
     (caseANegationPredicate      [node] (translate-to-lisb this sb "not" (.getPredicate node)))
+    (caseANotEqualPredicate      [node] (overwrite-appendstr this node "not="))
 
+    (caseAForallPredicate        [node] (translate-to-lisb this sb "forall" (.getIdentifiers node) (.getImplication node)))
+    (caseAExistsPredicate        [node] (translate-to-lisb this sb "exists" (.getIdentifiers node) (.getPredicate node)))
+
+    (caseAComprehensionSetExpression [node] (translate-to-lisb this sb "set" (.getIdentifiers node) (.getPredicates node)))
     (caseASubsetPredicate        [node] (overwrite-appendstr this node "subset"))
     (caseASubsetStrictPredicate  [node] (overwrite-appendstr this node "subset-strict"))
     (caseAMemberPredicate        [node] (overwrite-appendstr this node "member"))
@@ -63,21 +80,33 @@
     (caseAFinSubsetExpression    [node] (translate-to-lisb this sb "fin" (.getExpression node)))
     (caseAFin1SubsetExpression   [node] (translate-to-lisb this sb "fin1" (.getExpression node)))
     (caseACardExpression         [node] (translate-to-lisb this sb "count" (.getExpression node)))
+    (caseAGeneralUnionExpression [node] (translate-to-lisb this sb "unite-sets" (.getExpression node)))
+    (caseAGeneralIntersectionExpression [node] (translate-to-lisb this sb "intersect-sets" (.getExpression node)))
+    (caseAQuantifiedUnionExpression [node] (translate-to-lisb this sb "union-pe" (.getIdentifiers node) (.getPredicates node) (.getExpression node)))
+    (caseAQuantifiedIntersectionExpression [node] (translate-to-lisb this sb "intersection-pe" (.getIdentifiers node) (.getPredicates node) (.getExpression node)))
 
     (caseADomainExpression       [node] (translate-to-lisb this sb "dom" (.getExpression node)))
     (caseARangeExpression        [node] (translate-to-lisb this sb "ran" (.getExpression node)))
     (caseAImageExpression        [node] (translate-to-lisb this sb "image" (.getLeft node) (.getRight node)))
     (caseAReverseExpression      [node] (translate-to-lisb this sb "inverse" (.getExpression node)))
+    (caseAFirstProjectionExpression [node] (translate-to-lisb this sb "prj1" (.getExp1 node) (.getExp2 node)))
+    (caseASecondProjectionExpression [node] (translate-to-lisb this sb "prj2" (.getExp1 node) (.getExp2 node)))
+
+    (caseALambdaExpression       [node] (translate-to-lisb this sb "lambda" (.getIdentifiers node) (.getPredicate node) (.getExpression node)))
+    (caseAFunctionExpression     [node] (apply translate-to-lisb this sb "apply" (.getIdentifier node) (.getParameters node)))
     ))
 
-(defn fresh-visitor []
-  (get-visitor (StringBuffer.)))
+(defn fresh-visitor
+  ([] (get-visitor (StringBuffer.) #{}))
+  ([symbol-set] (get-visitor (StringBuffer.) symbol-set)))
 
-(defn parse-print [s]
-  (let [ast (de.be4.classicalb.core.parser.BParser/parse (str "#FORMULA " s))
-        v (fresh-visitor)]
-    (.apply ast v)
-    (.getPrettyPrint v)))
+(defn parse-print
+  ([s] (parse-print s #{}))
+  ([s symbols]
+    (let [ast (de.be4.classicalb.core.parser.BParser/parse (str "#FORMULA " s))
+          v (fresh-visitor symbols)]
+      (.apply ast v)
+      (.getPrettyPrint v))))
 
 (def parse-print-read
   (comp read-string parse-print))
--- test/lisb/AST2lisb_test.clj
+++ test/lisb/AST2lisb_test.clj
@@ -10,20 +10,24 @@
     (is (= true  (parse-print-read "TRUE")))
     (is (= false (parse-print-read "FALSE")))
     (is (= '(bpred->bool (b= 1 1)) (parse-print-read "bool(1=1)")))) 
+  (testing "identifiers"
+    (is (= '(b= a :b) (parse-print-read "a=b" #{"a"}))))
   (testing "sets"
     (is (= #{}    (parse-print-read "{}")))
-    (is (= '(bset-enum 1)    (parse-print-read "{1}"))) 
-    (is (= '(bset-enum 1 2)  (parse-print-read "{1,2}")))
-    (is (= '(bcouple 1 2)    (parse-print-read "1|->2")))
-    (is (= '(bbool-set)      (parse-print-read "BOOL")))
-    (is (= '(bnat-set)       (parse-print-read "NAT")))
-    (is (= '(bnat1-set)      (parse-print-read "NAT1")))
-    (is (= '(bnatural-set)   (parse-print-read "NATURAL")))
-    (is (= '(bnatural1-set)  (parse-print-read "NATURAL1")))
-    (is (= '(bint-set)       (parse-print-read "INT")))
-    (is (= '(binteger-set)   (parse-print-read "INTEGER"))))
+    (is (= '(bset-enum 1)         (parse-print-read "{1}"))) 
+    (is (= '(bset-enum 1 2)       (parse-print-read "{1,2}")))
+    (is (= '(bset [:x] (b< :x 5)) (parse-print-read "{x|x<5}")))
+    (is (= '(bcouple 1 2)         (parse-print-read "1|->2")))
+    (is (= '(bbool-set)           (parse-print-read "BOOL")))
+    (is (= '(bnat-set)            (parse-print-read "NAT")))
+    (is (= '(bnat1-set)           (parse-print-read "NAT1")))
+    (is (= '(bnatural-set)        (parse-print-read "NATURAL")))
+    (is (= '(bnatural1-set)       (parse-print-read "NATURAL1")))
+    (is (= '(bint-set)            (parse-print-read "INT")))
+    (is (= '(binteger-set)        (parse-print-read "INTEGER"))))
   (testing "some integer operations"
     (are [x y] (= x (parse-print-read y))
+      '(b- 1)      "-1"
       '(b+ 1 2)    "1+2"
       '(b- 1 2)    "1-2"
       '(b* 1 2)    "1*2"
@@ -35,10 +39,13 @@
       '(bmod 1 2)  "1 mod 2"
       '(b= 1 2)    "1=2"
       '(b** 1 2)   "1 ** 2"
-      '(b** 1 (b** 2 3)) "1 ** 2 ** 3")
-      '(bmax (benum-set 1 2)) "max({1,2})"
-      '(bmin (benum-set 1 2)) "min({1,2})"
-    )
+      '(b** 1 (b** 2 3)) "1 ** 2 ** 3"
+      '(binterval 1 5)    "1..5"
+      '(bmax (bset-enum 1 2)) "max({1,2})"
+      '(bmin (bset-enum 1 2)) "min({1,2})"
+      '(bsigma [:x] (b< :x 5) (b* :x :x)) "SIGMA(x).(x < 5|x*x)"
+      '(bpi [:x] (b< :x 5) (b* :x :x)) "PI(x).(x < 5|x*x)"
+      ))
   (testing "logical predicates"
     (are [x y] (= x (parse-print-read y))
       '(band (b= 1 1) (b= 1 2))   "(1=1) & (1=2)"
@@ -46,12 +53,17 @@
       '(b<=> (b= 1 1) (b= 1 2))   "(1=1) <=> (1=2)"
       '(b=> (b= 1 1) (b= 1 2))    "(1=1) => (1=2)"
       '(bnot (b= 1 1))            "not(1=1)"
+      '(bnot= 1 2)                "1 /= 2"
+      '(bforall [:x] (b=> (b= :x 1) (b= :x 1))) "!x.(x=1 => x=1)"
+      '(bforall [:x :y] (b=> (b= :x 1) (b= :y 1))) "!x,y.(x=1 => y=1)"
+      '(bexists [:x :y] (b=> (b= :x 1) (b= :y 1))) "#x,y.(x=1 => y=1)"
       ))
   (testing "sets operations"
     (are [x y] (= x (parse-print-read y))
       '(b* #{} #{})              "{} * {}"
       '(bunion #{} #{})          "{} \\/ {}"
       '(bintersection #{} #{})   "{} /\\ {}"
+;      '(b- #{} #{})              "{} - {}"  ;; FIXME: I don't know if I want this to happen
       '(bmember 1 (bset-enum 1)) "1 : {1}"
       '(bpow (bset-enum 1))      "POW({1})"
       '(bpow1 (bset-enum 1))     "POW1({1})"
@@ -60,6 +72,10 @@
       '(bsubset #{} (bset-enum 1)) "{} <: {1}"
       '(bsubset-strict #{} (bset-enum 1)) "{} <<: {1}"
       '(bcount (bset-enum 1))    "card({1})"
+      '(bunion-pe [:x] (b< 0 :x) (bset-enum :x (b* :x :x))) "UNION(x).(0 < x|{x, x*x})"
+      '(bintersection-pe [:x] (b< 0 :x) (bset-enum :x (b* :x :x))) "INTER(x).(0 < x|{x, x*x})"
+      '(bunite-sets (bset-enum (bset-enum 1) (bset-enum 2 3))) "union({{1}, {2,3}})"
+      '(bintersect-sets (bset-enum (bset-enum 1) (bset-enum 2 3))) "inter({{1}, {2,3}})"
       ))
   (testing "relations"
     (are [x y] (= x (parse-print-read y))
@@ -74,7 +90,8 @@
       '(bran #{})      "ran({})"
       '(bimage #{} (bset-enum 1)) "{}[{1}]"
       '(binverse #{})      "{}~"
-      
+      '(bprj1 (bset-enum 1) (bset-enum 3 4)) "prj1({1}, {3,4})"
+      '(bprj2 (bset-enum 1) (bset-enum 3 4)) "prj2({1}, {3,4})"
       ))
   (testing "functions"
     (are [x y] (= x (parse-print-read y))
@@ -85,5 +102,8 @@
       '(b>+> #{} #{})   "{} >+> {}"
       '(b>-> #{} #{})   "{} >-> {}"
       '(b>+>> #{} #{})  "{} >+>> {}"
-      '(b>->> #{} #{})  "{} >->> {}")))
+      '(b>->> #{} #{})  "{} >->> {}"
+      '(blambda [:x] (b< :x 2) (b* :x :x)) "%x.(x<2|x*x)"
+      '(bapply (bset-enum (bcouple 1 2)) 1) "{1|->2}(1)"
+      )))
  
