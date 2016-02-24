73279d3d12927652b10fb3a34d3fa33603d7ef0a
Jens Bendisposto <jens@bendisposto.de>
2016-02-24 16:27:06 +0100

    changed dep to release

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["snapshot" "https://oss.sonatype.org/content/repositories/snapshots"]]
+  :repositories [["snapshot" "https://oss.sonatype.org/content/repositories/snapshots"]
+                 ["release" "https://oss.sonatype.org/content/repositories/releases"]]
   :dependencies [[org.clojure/clojure "1.8.0-beta2"]
                  [org.clojure/math.combinatorics "0.1.1"]
-                 [de.hhu.stups/de.prob2.kernel,"2.0.0-SNAPSHOT"]])
+                 [de.hhu.stups/de.prob2.kernel,"2.0.0"]])
