f6f6f707509c238432bfed2dacb659c77efd90b4
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2017-05-23 14:44:08 +0200

    increase stack size, update options, make n = 4 work

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["snapshot" "https://oss.sonatype.org/content/repositories/snapshots"]
                  ["release" "https://oss.sonatype.org/content/repositories/releases"]]
+  :jvm-opts ["-Xss1g"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.3"]
                  [de.hhu.stups/de.prob2.kernel,"3.2.1"]])
