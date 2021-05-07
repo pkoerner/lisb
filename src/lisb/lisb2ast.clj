550859c764974d92c1e204c9a79475422bf4bed4
Mager, Florian Peter Leander (flmag100) <florian.mager@uni-duesseldorf.de>
2021-05-07 11:53:37 +0200

    Tests für lisb2ast_test geschrieben und translation erweitert.

--- src/lisb/lisb2ast.clj
+++ src/lisb/lisb2ast.clj
@@ -1,9 +1,18 @@
 (ns lisb.lisb2ast
-  (:require [lisb.representation :refer [lisb->node-repr]]
-            [lisb.translation :refer [node-repr->ast node-repr->predicate-ast]]))
+  (:require [lisb.representation :refer [lisb->node-repr, lisb->node-repr1]]
+            [lisb.translation :refer [node-repr->ast node-repr->predicate-ast node-repr->expression-ast node-repr->substitution-ast node-repr->machine-clause-ast]]))
 
 (defn lisb->ast [lisb]
   (node-repr->ast (lisb->node-repr lisb)))
 
 (defn lisb-predicate->ast [lisb]
   (node-repr->predicate-ast (lisb->node-repr lisb)))
+
+(defn lisb-expression->ast [lisb]
+  (node-repr->expression-ast (lisb->node-repr lisb)))
+
+(defn lisb-substitution->ast [lisb]
+  (node-repr->substitution-ast (lisb->node-repr lisb)))
+
+(defn lisb-machine-clause->ast [lisb]
+  (node-repr->machine-clause-ast (lisb->node-repr lisb)))
