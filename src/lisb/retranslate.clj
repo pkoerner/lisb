8cec5fbf5364db0c8a4f20074175ac89398b522e
Mager, Florian Peter Leander (flmag100) <florian.mager@uni-duesseldorf.de>
2021-09-08 17:42:55 +0200

    updated retranslate to new prob value-translator

--- src/lisb/retranslate.clj
+++ src/lisb/retranslate.clj
@@ -1,12 +1,33 @@
-(ns lisb.retranslate)
+(ns lisb.retranslate
+  (:import
+    (de.hhu.stups.prob.translator BAtom BBoolean BNumber BRecord BSet BString BTuple)
+    (de.hhu.stups.prob.translator.interpretations BFunction BRelation BSequence)))
 
 
 (defn retranslate [data]
   (condp instance? data
-    de.prob.translator.types.BigInteger (long data) ;; FIXME: could be a bigint...
-    de.prob.translator.types.Boolean    (.booleanValue data)
-    de.prob.translator.types.Sequence   (set (map retranslate data)) 
-    de.prob.translator.types.Set        (set (map retranslate data))
-    de.prob.translator.types.Tuple      (mapv retranslate data)
-    de.prob.translator.types.String     (.getValue data)
-    ))
+    ; value types
+    BAtom (.stringValue data)
+    BBoolean (.booleanValue data)
+    BNumber (.longValue data)
+    BString (.stringValue data)
+    ; interpreted collection types of set
+    BSequence (mapv retranslate (.toList data))
+    BFunction (reduce
+                (fn [m e]
+                  (assoc m (retranslate (.getKey e)) (retranslate (.getValue e))))
+                {}
+                (.toMap data))
+    BRelation (reduce
+                (fn [m e]
+                  (assoc m (retranslate (.getKey e)) (mapv retranslate (.getValue e))))
+                {}
+                (.toRelationalMap data))
+    ; collection types
+    BSet (set (map retranslate (.toSet data)))
+    BTuple [(.getFirst data) (.getSecond data)]
+    BRecord (reduce
+              (fn [m e]
+                (assoc m (retranslate (.getKey e)) (retranslate (.getValue e))))
+              {}
+              (.toMap data))))
