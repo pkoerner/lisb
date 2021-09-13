32c5fcea141b901eba2d77e4f364d82f1746a9fc
Kristin Rutenkolk <kristin.rutenkolk@hhu.de>
2021-09-13 21:53:13 +0200

    add draft for snake example

--- src/lisb/examples/simple.clj
+++ src/lisb/examples/simple.clj
@@ -109,6 +109,22 @@
   (possible-ops smalltrace))
 
 (comment
+  this wants to be a snake game example when grown up.
+  (def snek (b (machine
+               (machine-variant)
+               (machine-header :Snek [])
+               (variables :board :direction)
+               (invariant
+                (and
+                                        ;types
+                 (member :board (total-fn
+                                 (* (range 1 9) (range 1 9))
+                                 {0 1}))
+                 (member :direction (* (range -1 1) (range -1 1)))))
+               (init (assign :board )) ; oh. how do i do set comprehension?
+               ))))
+
+(comment
 
   (perform smalltrace (.getName (first smalltrace-next-steps)))
 
