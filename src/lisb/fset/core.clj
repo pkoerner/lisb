0dae246b823cfda9a422e4aa67eccee78b6c9c77
JanRossbach <janrossbach3@gmail.com>
2021-09-15 14:34:09 +0200

    Add meta-data support for fset

--- src/lisb/fset/core.clj
+++ src/lisb/fset/core.clj
@@ -6,7 +6,7 @@
    [lisb.high-level :refer [load-mch! save-mch! make-mch!]]
    [lisb.fset.transform :refer [transform]]))
 
-;; This is the entry point.
+;; This is the entry point for the module.
 
 ;; define working Directories
 (def source-dir "resources/machines/fset-source/")
@@ -16,21 +16,26 @@
 ;; Prefix of the resulting files after running transform-save-machines!
 (def prefix "rw_") ;
 
+;; Set the metadata for the Translation Process.
+(def meta-data {:deferred-size 3})
+
 ;; Specify a List of Machines if you don't want all of them to transform.
 (def machines-to-transform
   '("Lift.mch"
-    ;"scheduler.mch"
+    ; "scheduler.mch"
     ))
 
 (defn load-transform-machine!
   [source-filename]
-  (transform (load-mch! source-filename)))
+  (transform (load-mch! source-filename meta-data)))
 
 (defn load-transform-save-machine!
   [source-filename target-filename]
-  (save-mch! (load-transform-machine! source-filename) target-filename))
+  (save-mch! (:ir (load-transform-machine! source-filename)) target-filename))
 
 (defn transform-machines!
+  "Transforms all or just a list of the B machines in the set source directory and
+   returns a list of the IR's"
   ([]
    (let [machines (.list (io/file source-dir))]
      (for [m machines]
@@ -40,6 +45,8 @@
      (load-transform-machine! (str source-dir m)))))
 
 (defn transform-save-machines!
+  "Transforms all or just a list of the B machines in the set source directory and
+   saves the as files in the set target directory."
   ([]
    (let [machines (.list (io/file source-dir))]
      (for [m machines]
@@ -51,7 +58,7 @@
 (defn print-transform!
   "Takes a machine in IR and pprints it and its transformation."
   [machine]
-  (let [m (make-mch! machine)
+  (let [m (make-mch! machine meta-data)
         transformed-machine (:ir (transform m))]
     (pprint "--------------------------")
     (pprint machine)
@@ -63,10 +70,10 @@
 
   (transform-machines! machines-to-transform)
 
-  (transform-machines)
+  (transform-machines!)
+
+  (transform-save-machines!)
 
   (transform-save-machines! machines-to-transform)
-
-  (transform-save-machines)
 
 )
