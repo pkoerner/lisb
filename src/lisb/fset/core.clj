9cbe32ca4c62af878c000c7aa32eff3e3285d0f2
JanRossbach <janrossbach3@gmail.com>
2021-09-15 10:08:05 +0200

    Add fset namespace

---
+++ src/lisb/fset/core.clj
@@ -0,0 +1,61 @@
+(ns lisb.fset.core
+  (:require
+   [clojure.java.io :as io]
+   [lisb.core :refer [eval-ir-as-predicate eval-ir-as-expression]]
+   [clojure.pprint :refer [pprint]]
+   [lisb.high-level :refer [load-mch! save-mch!]]
+   [lisb.fset.transform :refer [transform]]))
+
+;; This is the entry point.
+
+;; define working Directories
+(def source-dir "resources/machines/transform-source/")
+(def target-dir "resources/machines/transform-target/")
+
+;; Prefix
+(def prefix "rw_") ;
+
+;; Specify a List of Machines if you don't want all of them to transform.
+(def machines-to-transform
+  '("Lift.mch"
+    ;"scheduler.mch"
+    ))
+
+(defn load-transform-machine!
+  [source-filename]
+  (transform (load-mch! source-filename)))
+
+(defn load-transform-save-machine!
+  [source-filename target-filename]
+  (save-mch! (load-transform-machine! source-filename) target-filename))
+
+(defn transform-machines
+  ([]
+   (let [machines (.list (io/file source-dir))]
+     (for [m machines]
+       (load-transform-machine! (str source-dir m)))))
+  ([machines]
+   (for [m machines]
+     (load-transform-machine! (str source-dir m)))))
+
+(defn transform-save-machines!
+  ([]
+   (let [machines (.list (io/file source-dir))]
+     (for [m machines]
+       (load-transform-save-machine! (str source-dir m) (str target-dir prefix m)))))
+  ([machines]
+   (for [m machines]
+     (load-transform-save-machine! (str source-dir m) (str target-dir prefix m)))))
+
+
+(comment
+
+  (transform-machines! machines-to-transform)
+
+  (transform-machines)
+
+  (transform-save-machines! machines-to-transform)
+
+  (transform-save-machines)
+
+)
---
+++ src/lisb/fset/transform.clj
@@ -0,0 +1,36 @@
+(ns lisb.fset.transform
+  (:require
+   [com.rpl.specter :as s]
+   [lisb.core :refer [eval-ir-as-predicate eval-ir-as-expression]]))
+
+(defn get-node-by-tag
+  [el-tag ir]
+  (s/select [(s/walker #(= (:tag %) el-tag))] ir))
+
+(defn update-node-by-tag
+  [el-tag update-fn m]
+  (s/transform [:ir (s/walker #(= (:tag %) el-tag))] update-fn m))
+
+(defn transform-sets
+  [m]
+  m)
+
+(defn transform-definitions
+  [m]
+  m)
+
+(defn transform-invariant
+  [m]
+  m)
+
+(defn transform-operations
+  [m]
+  m)
+
+(defn transform
+  [m]
+  (->> (assoc m :meta {})
+       (transform-sets)
+       (transform-definitions)
+       (transform-invariant)
+       (transform-operations)))
