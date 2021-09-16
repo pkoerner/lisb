a8c0f154acdcecbab5110b094914fe43106644b5
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2021-09-16 11:06:53 +0200

    Problem: no deploy mechanism
    Solution: fix project.clj to include basic info and the proper Clojars group

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lisb "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.pkoerner/lisb "0.0.1-SNAPSHOT"
+  :description "lisb: A Clojure library that wraps ProB's constraint solver. Use it to write/transform B machines with less pain or interact with the constraint solver."
+  :url "https://gitlab.cs.uni-duesseldorf.de/mager/lisb-clone" ;; subject to change
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["snapshot" "https://oss.sonatype.org/content/repositories/snapshots"]
@@ -12,5 +12,4 @@
                  [de.hhu.stups/de.prob2.kernel,"3.14.0"]
                  [de.hhu.stups/value-translator "0.1.2"]
                  [de.hhu.stups/bparser "2.9.28"]
-                 [clj-wallhack "1.0.1"]
-                 ])
+                 [clj-wallhack "1.0.1"]])
