907b9f5e49981b29634974018e73c781598258ee
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2021-09-17 10:14:15 +0200

    Problem: cannot deploy without setting up GPG to sign the .jar file
    Solution: nobody knows my key anyway, so no signing for now

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["snapshot" "https://oss.sonatype.org/content/repositories/snapshots"]
                  ["release" "https://oss.sonatype.org/content/repositories/releases"]]
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
   :jvm-opts ["-Xss1g"]
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/math.combinatorics "0.1.3"]
