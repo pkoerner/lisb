3cba433ab8901dcd191e19f86318034b751756f6
Mager, Florian Peter Leander (flmag100) <florian.mager@uni-duesseldorf.de>
2021-11-05 14:18:55 +0100

    add #{ids | pred} as comprehension-set lisb-dsl sugar.

--- src/lisb/translation/lisb2ir.clj
+++ src/lisb/translation/lisb2ir.clj
@@ -1,5 +1,6 @@
 (ns lisb.translation.lisb2ir
-  (:require [clojure.math.combinatorics :refer [combinations]]))
+  (:require [clojure.math.combinatorics :refer [combinations]])
+  (:require [clojure.walk :refer [walk]]))
 
 
 (declare b=)
@@ -796,247 +797,263 @@
 (defn bmap-set [p s]
   (bran (blambda [:x] (bmember? :x s) (p :x))))
 
-; | -> comp-set
-; 3-stellig
-; keyword or vector -> identifier[s]
 
+(defn is-keyword-or-vector [e]
+  (or
+    (= clojure.lang.PersistentVector (type e))
+    (= clojure.lang.Keyword (type e))))
+
+(defn process-comprehension-set [lisb]
+  (if (and (set? lisb) (contains? lisb '|))
+    (if (= 3 (count lisb))
+      (let [lisb (disj lisb '|)
+            [ids pred] (cond
+                         (is-keyword-or-vector (first lisb)) [(first lisb) (second lisb)]
+                         (is-keyword-or-vector (second lisb)) [(second lisb) (first lisb)]
+                         :else (throw (str "Unsupported way of set comprehension!" lisb)))]
+        (conj (list ids pred) 'comprehension-set))
+      (throw (str "Unsupported way of set comprehension!" lisb)))
+    (if (seqable? lisb)
+      (walk process-comprehension-set identity lisb)
+      lisb)))
 
 (defmacro b [lisb]
-  `(let [
-         ; parse units
-         ~'machine bmachine
-         ~'model bmodel
-         ~'system bsystem
-         ~'refinement brefinement
-         ~'implementation bimplementation
+  (let [removed-pipes (process-comprehension-set lisb)]
+    `(let [
+           ; parse units
+           ~'machine bmachine
+           ~'model bmodel
+           ~'system bsystem
+           ~'refinement brefinement
+           ~'implementation bimplementation
 
-         ; machine clauses
-         ; machine inclusions
-         ~'uses buses
-         ~'includes bincludes
-         ~'sees bsees
-         ~'extends bextends
-         ~'promotes bpromotes
-         ; machine sections
-         ~'constraints bconstraints
-         ~'sets bsets
-         ~'deferred-set bdeferred-set
-         ~'enumerated-set benumerated-set
-         ~'constants bconstants
-         ~'properties bproperties
-         ~'definitions bdefinitions
-         ~'variables bvariables
-         ~'invariants binvariants
-         ~'assertions bassertions
-         ~'init binit
-         ~'operations boperations
-         ~'operation boperation
+           ; machine clauses
+           ; machine inclusions
+           ~'uses buses
+           ~'includes bincludes
+           ~'sees bsees
+           ~'extends bextends
+           ~'promotes bpromotes
+           ; machine sections
+           ~'constraints bconstraints
+           ~'sets bsets
+           ~'deferred-set bdeferred-set
+           ~'enumerated-set benumerated-set
+           ~'constants bconstants
+           ~'properties bproperties
+           ~'definitions bdefinitions
+           ~'variables bvariables
+           ~'invariants binvariants
+           ~'assertions bassertions
+           ~'init binit
+           ~'operations boperations
+           ~'operation boperation
 
-         ; substitutions
-         ~'skip bskip
-         ~'block bblock
-         ~'assign bassign
-         ~'set! bassign                                     ; sugar
-         ~'becomes-element-of bbecomes-element-of
-         ~'becomes-member bbecomes-element-of               ; sugar
-         ~'becomes-such bbecomes-such
-         ~'op-call bop-call
-         ~'parallel-sub bparallel-sub
-         ~'|| bparallel-sub                                 ; sugar
-         ~'sequential-sub bsequential-sub
-         ~'any bany
-         ~'let-sub blet-sub
-         ~'var-sub bvar
-         ~'pre bprecondition
-         ~'assert bassert
-         ~'choice bchoice
-         ~'if-sub bif-sub
-         ~'cond bcond
-         ~'select bselect
-         ~'case bcase
-         ~'op-sub bop-sub
+           ; substitutions
+           ~'skip bskip
+           ~'block bblock
+           ~'assign bassign
+           ~'set! bassign                                   ; sugar
+           ~'becomes-element-of bbecomes-element-of
+           ~'becomes-member bbecomes-element-of             ; sugar
+           ~'becomes-such bbecomes-such
+           ~'op-call bop-call
+           ~'parallel-sub bparallel-sub
+           ~'|| bparallel-sub                               ; sugar
+           ~'sequential-sub bsequential-sub
+           ~'any bany
+           ~'let-sub blet-sub
+           ~'var-sub bvar
+           ~'pre bprecondition
+           ~'assert bassert
+           ~'choice bchoice
+           ~'if-sub bif-sub
+           ~'cond bcond
+           ~'select bselect
+           ~'case bcase
+           ~'op-sub bop-sub
 
-         ; if
-         ~'if-expr bif-expr
+           ; if
+           ~'if-expr bif-expr
 
-         ; let
-         ~'let blet
+           ; let
+           ~'let blet
 
-         ; trees
+           ; trees
 
-         ; reals - (alpha - besser nicht verwenden)
+           ; reals - (alpha - besser nicht verwenden)
 
-         ; strings
-         ~'string-set bstring-set
+           ; strings
+           ~'string-set bstring-set
 
-         ; records
-         ~'struct bstruct
-         ~'record brecord
-         ~'record-get brecord-get
+           ; records
+           ~'struct bstruct
+           ~'record brecord
+           ~'record-get brecord-get
 
-         ; sequences
-         ~'sequence bsequence
-         ~'seq bseq
-         ~'seq1 bseq1
-         ~'iseq biseq
-         ~'iseq1 biseq1
-         ~'perm bperm
-         ~'size bsize
-         ~'concat bconcat
-         ~'prepend bprepend
-         ~'-> bprepend                                      ; sugar
-         ~'append bappend
-         ~'<- bappend                                       ; sugar
-         ~'reverse breverse
-         ~'first bfirst
-         ~'last blast
-         ~'front bfront
-         ~'drop-last bfront                                 ; clojure
-         ~'tail btail
-         ~'rest btail                                       ; clojure
-         ~'conc bconc
-         ~'take btake
-         ~'drop bdrop
+           ; sequences
+           ~'sequence bsequence
+           ~'seq bseq
+           ~'seq1 bseq1
+           ~'iseq biseq
+           ~'iseq1 biseq1
+           ~'perm bperm
+           ~'size bsize
+           ~'concat bconcat
+           ~'prepend bprepend
+           ~'-> bprepend                                    ; sugar
+           ~'append bappend
+           ~'<- bappend                                     ; sugar
+           ~'reverse breverse
+           ~'first bfirst
+           ~'last blast
+           ~'front bfront
+           ~'drop-last bfront                               ; clojure
+           ~'tail btail
+           ~'rest btail                                     ; clojure
+           ~'conc bconc
+           ~'take btake
+           ~'drop bdrop
 
-         ; functions
-         ~'partial-function bpartial-function
-         ~'+-> bpartial-function                                  ; sugar
-         ~'total-function btotal-function
-         ~'--> btotal-function
-         ~'partial-surjection bpartial-surjection
-         ~'+->> bpartial-surjection
-         ~'total-surjection btotal-surjection
-         ~'-->> btotal-surjection
-         ~'partial-injection bpartial-injection
-         ~'>+> bpartial-injection
-         ~'total-injection btotal-injection
-         ~'>-> btotal-injection
-         ~'partial-bijection bpartial-bijection
-         ~'>+>> bpartial-bijection
-         ~'total-bijection btotal-bijection
-         ~'>->> btotal-bijection
-         ~'lambda blambda
-         ;~'fn bfn  ; sugar
-         ~'fn-call bfn-call
+           ; functions
+           ~'partial-function bpartial-function
+           ~'+-> bpartial-function                          ; sugar
+           ~'total-function btotal-function
+           ~'--> btotal-function
+           ~'partial-surjection bpartial-surjection
+           ~'+->> bpartial-surjection
+           ~'total-surjection btotal-surjection
+           ~'-->> btotal-surjection
+           ~'partial-injection bpartial-injection
+           ~'>+> bpartial-injection
+           ~'total-injection btotal-injection
+           ~'>-> btotal-injection
+           ~'partial-bijection bpartial-bijection
+           ~'>+>> bpartial-bijection
+           ~'total-bijection btotal-bijection
+           ~'>->> btotal-bijection
+           ~'lambda blambda
+           ;~'fn bfn  ; sugar
+           ~'fn-call bfn-call
 
-         ; relations
-         ~'relation brelation
-         ~'<-> brelation                                    ; sugar
-         ~'total-relation btotal-relation
-         ~'<<-> btotal-relation                             ; sugar
-         ~'surjective-relation bsurjective-relation
-         ~'<->> bsurjective-relation                        ; sugar
-         ~'total-surjective-relation btotal-surjective-relation
-         ~'<<->> btotal-surjective-relation                 ; sugar
-         ~'maplet bmaplet
-         ~'|-> bmaplet                                      ; sugar
-         ~'dom bdom
-         ~'ran bran
-         ~'id bid
-         ~'domain-restriction bdomain-restriction
-         ~'<| bdomain-restriction                           ; sugar
-         ~'domain-subtraction bdomain-subtraction
-         ~'<<| bdomain-subtraction                          ; sugar
-         ~'range-restriction brange-restriction
-         ~'|> brange-restriction                            ; sugar
-         ~'range-subtraction brange-subtraction
-         ~'|>> brange-subtraction                           ; sugar
-         ~'inverse binverse
-         ~'image bimage
-         ~'override boverride
-         ~'<+ boverride                                     ; sugar
-         ~'direct-product bdirect-product
-         ~'>< bdirect-product                               ; sugar
-         ~'composition bcomposition
-         ~'parallel-product bparallel-product
-         ~'prj1 bprj1
-         ~'prj2 bprj2
-         ~'closure1 bclosure1
-         ~'closure bclosure
-         ~'iterate biterate
-         ~'fnc bfnc
-         ~'rel brel
+           ; relations
+           ~'relation brelation
+           ~'<-> brelation                                  ; sugar
+           ~'total-relation btotal-relation
+           ~'<<-> btotal-relation                           ; sugar
+           ~'surjective-relation bsurjective-relation
+           ~'<->> bsurjective-relation                      ; sugar
+           ~'total-surjective-relation btotal-surjective-relation
+           ~'<<->> btotal-surjective-relation               ; sugar
+           ~'maplet bmaplet
+           ~'|-> bmaplet                                    ; sugar
+           ~'dom bdom
+           ~'ran bran
+           ~'id bid
+           ~'domain-restriction bdomain-restriction
+           ~'<| bdomain-restriction                         ; sugar
+           ~'domain-subtraction bdomain-subtraction
+           ~'<<| bdomain-subtraction                        ; sugar
+           ~'range-restriction brange-restriction
+           ~'|> brange-restriction                          ; sugar
+           ~'range-subtraction brange-subtraction
+           ~'|>> brange-subtraction                         ; sugar
+           ~'inverse binverse
+           ~'image bimage
+           ~'override boverride
+           ~'<+ boverride                                   ; sugar
+           ~'direct-product bdirect-product
+           ~'>< bdirect-product                             ; sugar
+           ~'composition bcomposition
+           ~'parallel-product bparallel-product
+           ~'prj1 bprj1
+           ~'prj2 bprj2
+           ~'closure1 bclosure1
+           ~'closure bclosure
+           ~'iterate biterate
+           ~'fnc bfnc
+           ~'rel brel
 
-         ; numbers
-         ~'integer-set binteger-set
-         ~'natural-set bnatural-set
-         ~'natural1-set bnatural1-set
-         ~'int-set bint-set
-         ~'nat-set bnat-set
-         ~'nat1-set bnat1-set
-         ~'interval binterval
-         ~'range brange
-         ~'min-int bmin-int
-         ~'max-int bmax-int
-         ~'> b>
-         ~'< b<
-         ~'>= b>=
-         ~'<= b<=
-         ~'max bmax
-         ~'min bmin
-         ~'+ b+
-         ~'- b-
-         ~'mul-or-cart bmul-or-cart
-         ~'* b*                                             ; added separat multiplication
-         ~'div bdiv
-         ~'/ bdiv
-         ~'** b**
-         ~'mod bmod
-         ~'pi bpi
-         ~'π bpi                                            ; sugar
-         ~'sigma bsigma
-         ~'Σ bsigma                                         ; sugar
-         ~'successor bsuccessor
-         ~'inc bsuccessor                                   ; sugar
-         ~'predecessor  bpredecessor
-         ~'dec bpredecessor                                 ; sugar
+           ; numbers
+           ~'integer-set binteger-set
+           ~'natural-set bnatural-set
+           ~'natural1-set bnatural1-set
+           ~'int-set bint-set
+           ~'nat-set bnat-set
+           ~'nat1-set bnat1-set
+           ~'interval binterval
+           ~'range brange
+           ~'min-int bmin-int
+           ~'max-int bmax-int
+           ~'> b>
+           ~'< b<
+           ~'>= b>=
+           ~'<= b<=
+           ~'max bmax
+           ~'min bmin
+           ~'+ b+
+           ~'- b-
+           ~'mul-or-cart bmul-or-cart
+           ~'* b*                                           ; added separat multiplication
+           ~'div bdiv
+           ~'/ bdiv
+           ~'** b**
+           ~'mod bmod
+           ~'pi bpi
+           ~'π bpi                                          ; sugar
+           ~'sigma bsigma
+           ~'Σ bsigma                                       ; sugar
+           ~'successor bsuccessor
+           ~'inc bsuccessor                                 ; sugar
+           ~'predecessor bpredecessor
+           ~'dec bpredecessor                               ; sugar
 
-         ;;; sets
-         ~'comprehension-set bcomprehension-set
-         ~'pow bpow
-         ~'pow1 bpow1
-         ~'fin bfin
-         ~'fin1 bfin1
-         ~'card bcard
-         ~'cartesian-product bcartesian-product             ; added separat cartesian-product
-         ; TODO: rebind ids in def-pred
-         ;~'x bcartesian-product                             ; sugar
-         ~'union bunion
-         ~'intersection bintersection
-         ~'set- bset-
-         ~'member? bmember?
-         ~'in bmember?                                      ; sugar
-         ~'contains? bcontains?                             ; sugar
-         ~'subset? bsubset?
-         ~'strict-subset? bstrict-subset?
-         ~'superset? bsuperset?                             ; sugar
-         ~'strict-superset? bstrict-superset?               ; sugar
-         ~'unite-sets bunite-sets
-         ~'intersect-sets bintersect-sets
-         ~'union-pe bunion-pe
-         ~'intersection-pe bintersection-pe
+           ;;; sets
+           ~'comprehension-set bcomprehension-set
+           ~'pow bpow
+           ~'pow1 bpow1
+           ~'fin bfin
+           ~'fin1 bfin1
+           ~'card bcard
+           ~'cartesian-product bcartesian-product           ; added separat cartesian-product
+           ; TODO: rebind ids in def-pred
+           ;~'x bcartesian-product                             ; sugar
+           ~'union bunion
+           ~'intersection bintersection
+           ~'set- bset-
+           ~'member? bmember?
+           ~'in bmember?                                    ; sugar
+           ~'contains? bcontains?                           ; sugar
+           ~'subset? bsubset?
+           ~'strict-subset? bstrict-subset?
+           ~'superset? bsuperset?                           ; sugar
+           ~'strict-superset? bstrict-superset?             ; sugar
+           ~'unite-sets bunite-sets
+           ~'intersect-sets bintersect-sets
+           ~'union-pe bunion-pe
+           ~'intersection-pe bintersection-pe
 
-         ;;; booleans
-         ~'bool-set bbool-set
-         ~'pred->bool bpred->bool
+           ;;; booleans
+           ~'bool-set bbool-set
+           ~'pred->bool bpred->bool
 
-         ;;; equality predicates
-         ~'= b=
-         ~'not= bnot=
-         ~'distinct? bdistinct?                             ; added funcionality
+           ;;; equality predicates
+           ~'= b=
+           ~'not= bnot=
+           ~'distinct? bdistinct?                           ; added funcionality
 
-         ;;; logical predicates
-         ~'and band
-         ~'or bor
-         ~'equivalence bequivalence
-         ~'<=> bequivalence                                 ; sugar
-         ~'implication bimplication
-         ~'=> bimplication                                  ; sugar
-         ~'not bnot
-         ~'for-all bfor-all
-         ~'exists bexists]
-     ~lisb
-     ))
+           ;;; logical predicates
+           ~'and band
+           ~'or bor
+           ~'equivalence bequivalence
+           ~'<=> bequivalence                               ; sugar
+           ~'implication bimplication
+           ~'=> bimplication                                ; sugar
+           ~'not bnot
+           ~'for-all bfor-all
+           ~'exists bexists]
+       ~removed-pipes
+       )))
 
 (defn lisb->ir [lisb]
   (eval `(b ~lisb)))
