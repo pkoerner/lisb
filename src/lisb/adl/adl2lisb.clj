cdc44f2d8d68ac261fc9adbecd75fb432a5e9060
Mager, Florian Peter Leander (flmag100) <florian.mager@uni-duesseldorf.de>
2021-11-15 16:35:22 +0100

    add var for adl

--- src/lisb/adl/adl2lisb.clj
+++ src/lisb/adl/adl2lisb.clj
@@ -34,8 +34,8 @@
 
 
 (defn while [pc condition & body]
-  (let [opname-enter (keyword (str "while_enter" pc))
-        opname-exit (keyword (str "while_exit" pc))
+  (let [opname-enter (keyword (str "while-enter" pc))
+        opname-exit (keyword (str "while-exit" pc))
         body-pc (inc pc)
         res (apply do body-pc body)]
     {:pc         (:pc res)
@@ -54,8 +54,8 @@
 (defn if 
   ([pc condition then] (apply if pc condition then [nil]))
   ([pc condition then else]
-   (let [opname-then (keyword (str "ifte-then" pc))
-         opname-else (keyword (str "ifte-else" pc))
+   (let [opname-then (keyword (str "if-then" pc))
+         opname-else (keyword (str "if-else" pc))
          then-pc (inc pc)
          then-res (apply do then-pc [then])
          else-pc (:pc then-res)
@@ -78,191 +78,29 @@
         operations ((:ops res) nil)
         invariants (:invariants res)]
     `{:invariants ~invariants
-      :operations ~operations}
-    #_`(b (apply boperations ~ops))))
+      :operations ~operations}))
 
-(defmacro adl [namey & args]
-  (let [vardecls (butlast args)
-        algorythm (last args)
-    algensalat (:ops (macroexpand algorythm))]
-    `(b (apply boperations ~algensalat))))
+(defn var [var]
+  {:variables  [(first var)]
+   :invariants [`(b ~(second var))]
+   :init       [`(bassign ~(first var) (b ~(nth var 2)))]})
 
+(defn process-decls [decls]
+  (loop [[decl & decls] decls
+         m {}]
+    (if decl
+      (recur decls (merge-with into m ((resolve (first decl)) (rest decl))))
+      m)))
 
-#_(clojure.pprint/pprint (macroexpand '(algorithm
-             (while (> :x 0)
-               (assign :x (/ :x 2) :y (* :y 2))
-               (if (not= 0 (mod :x 2))
-                 (assign :p (+ :p :y)))
-               ))))
+(defn build-machine [name clauses]
+  `(bmachine
+     ~name
+     (bvariables ~@(:variables clauses))
+     (binvariants ~@(:invariants clauses))
+     (binit ~@(:init clauses))
+     (boperations ~@(:operations clauses)) ))
 
-
-
-
-
-(comment 
-(def programm-counter (atom 0))
-
-(defn get-free-pc []
-  (swap! programm-counter inc))
-
-(defn process-assign [assign machine-ctx pc next-pc]
-  (let [assign-counter (:assign-counter machine-ctx)]
-    (->
-      machine-ctx
-      (update :assign-counter inc)
-      (update :operations conj (list
-                                 (keyword (str "assign" assign-counter))
-                                 []
-                                 (list 'pre
-                                       (list '= :pc pc)
-                                       (list 'sequential-sub
-                                             assign
-                                             (list 'assign :pc next-pc))))))))
-
-(declare process-statement)
-(declare process-statements)
-
-(defn process-if [if machine-ctx pc next-pc]
-  (let [if-counter (:if-counter machine-ctx)
-        next-machine-ctx (update machine-ctx :if-counter inc)
-        condition (nth if 1)
-        then (nth if 2)
-        then-pc (get-free-pc)]
-    (if (= 3 (count if))
-           ; if-then
-           (let [next-machine-ctx (update next-machine-ctx :operations conj
-                                          (list
-                                            (keyword (str "if" if-counter "_then"))
-                                            []
-                                            (list 'pre
-                                                  (list 'and
-                                                        (list '= :pc pc)
-                                                        condition)
-                                                  (list 'assign :pc then-pc)))
-                                          (list
-                                            (keyword (str "if" if-counter "_else"))
-                                            []
-                                            (list 'pre
-                                                  (list 'and
-                                                        (list '= :pc pc)
-                                                        (list 'not condition))
-                                                  (list 'assign :pc next-pc))))
-                 next-machine-ctx (process-statement then next-machine-ctx then-pc next-pc)]
-             next-machine-ctx)
-           ; if-then-else
-           (let [next-machine-ctx (update next-machine-ctx :operations conj
-                                          (list
-                                            (keyword (str "if" if-counter "_then"))
-                                            []
-                                            (list 'pre
-                                                  (list 'and
-                                                        (list '= :pc pc)
-                                                        condition)
-                                                  (list 'assign :pc then-pc))))
-                 next-machine-ctx (process-statement then next-machine-ctx then-pc next-pc)
-                 else (nth if 3)
-                 else-pc (get-free-pc)
-                 next-machine-ctx (update next-machine-ctx :operations conj
-                                          (list
-                                            (keyword (str "if" if-counter "_else"))
-                                            []
-                                            (list 'pre
-                                                  (list 'and
-                                                        (list '= :pc pc)
-                                                        condition)
-                                                  (list 'assign :pc else-pc))))
-                 next-machine-ctx (process-statement else next-machine-ctx else-pc next-pc)]
-             next-machine-ctx))))
-
-(defn process-while [while machine-ctx pc next-pc]
-  (let [while-counter (:while-counter machine-ctx)
-        next-machine-ctx (update machine-ctx :while-counter inc)
-        condition (nth while 1)
-        body-pc (get-free-pc)
-        next-machine-ctx (update next-machine-ctx :operations conj
-                                 (list
-                                   (keyword (str "while" while-counter "_enter"))
-                                    []
-                                   (list 'pre
-                                         (list 'and
-                                               (list '= :pc pc)
-                                               condition)
-                                         (list 'assign :pc body-pc)))
-                                 (list
-                                   (keyword (str "while" while-counter "_exit"))
-                                   []
-                                   (list 'pre
-                                         (list 'and
-                                               (list '= :pc pc)
-                                               (list 'not condition))
-                                         (list 'assign :pc next-pc))))
-        next-machine-ctx (process-statements (drop 2 while) next-machine-ctx body-pc pc)]
-    next-machine-ctx))
-
-(defn process-statement [statement machine-ctx pc next-pc]
-  (cond
-    (= 'assign (first statement)) (process-assign statement machine-ctx pc next-pc)
-    (= 'if (first statement)) (process-if statement machine-ctx pc next-pc)
-    (= 'while (first statement)) (process-while statement machine-ctx pc next-pc)
-    (= 'do (first statement)) (process-statements (rest statement) machine-ctx pc next-pc)))
-
-(defn process-statements
-  ([statements machine-ctx]
-   (reset! programm-counter 0)
-   (process-statements statements machine-ctx 0))
-  ([statements machine-ctx pc]
-   (if (empty? statements)
-     machine-ctx
-     (let [next-pc (get-free-pc)
-           next-machine-ctx (process-statement (first statements) machine-ctx pc next-pc)]
-       (recur (rest statements) next-machine-ctx next-pc))))
-  ([statements machine-ctx pc last-pc]
-   (if (= 1 (count statements))
-     (let [last (first statements)]
-       (process-statement last machine-ctx pc last-pc))
-     (let [next-pc (get-free-pc)
-           next-machine-ctx (process-statement (first statements) machine-ctx pc next-pc)]
-       (recur (rest statements) next-machine-ctx next-pc last-pc)))))
-
-
-(defn process-var [var machine-ctx]
-  (-> machine-ctx
-      (update :variables conj (nth var 1))
-      (update :invariants conj (nth var 2))
-      (update :init conj (list 'assign (nth var 1) (nth var 3)))))
-
-(defn process-adl [definitions machine-ctx]
-  (if (empty? definitions)
-    machine-ctx
-    (let [definition (first definitions)
-          new-machine-ctx (cond
-                        (= 'var (first definition)) (process-var definition machine-ctx)
-                        (= 'algorithm (first definition)) (process-statements (rest definition) machine-ctx)
-                        )]
-      (recur (rest definitions) new-machine-ctx))))
-
-(defn create-machine-ctx [adl]
-  (assert (= 'adl (first adl)) "Algorithm-Definition-Language should look like (adl ...)")
-  (process-adl (drop 2 adl) {:name (second adl)
-                             :variables  [:pc]
-                             :invariants ['(in :pc nat-set)]
-                             :init       ['(assign :pc 0)]
-                             :operations []
-                             :assign-counter 0
-                             :if-counter 0
-                             :while-counter 0
-                             :do-counter 0}))
-
-(defn build-machine [machine-ctx]
-  (list 'machine
-        (:name machine-ctx)
-        (list* 'variables (:variables machine-ctx))
-        (list* 'invariants (:invariants machine-ctx))
-        (list* 'init (:init machine-ctx))
-        (list* 'operations (:operations machine-ctx)) ))
-
-(defn adl->lisb [adl]
-  (-> adl
-    (create-machine-ctx)
-    (build-machine)))
-)
+(defmacro adl [name & args]
+  (let [decls (process-decls (cons `(var :pc (bmember? :pc bnat-set) 0) (butlast args)))
+        algorythm (macroexpand (last args))]
+    (build-machine name (merge-with into decls algorythm))))
--- test/lisb/adl/adl2lisb_test.clj
+++ test/lisb/adl/adl2lisb_test.clj
@@ -3,91 +3,79 @@
   (:require [lisb.adl.adl2lisb :refer :all])
   (:require [lisb.translation.lisb2ir :refer :all]))
 
-#_(deftest multiply-example-test
-  (testing "multiply-example"
-    (is (= '(machine
-              :Multiply
-              (variables :pc :x :y :p)
-              (invariants (in :pc nat-set) (in :x nat-set) (in :y nat-set) (in :p nat-set))
-              (init (assign :pc 0) (assign :x 5) (assign :y 3) (assign :p 0))
-              (operations
-                (:while0_enter [] (pre (and (= :pc 0) (> :x 0)) (assign :pc 2)))
-                (:while0_exit [] (pre (and (= :pc 0) (not (> :x 0))) (assign :pc 1)))
-                (:if0_then [] (pre (and (= :pc 2) (not= 0 (mod :x 2))) (assign :pc 4)))
-                (:if0_else [] (pre (and (= :pc 2) (not (not= 0 (mod :x 2)))) (assign :pc 3)))
-                (:assign0 [] (pre (= :pc 4) (sequential-sub (assign :p (+ :p :y)) (assign :pc 3))))
-                (:assign1 [] (pre (= :pc 3) (sequential-sub (assign :x (/ :x 2) :y (* :y 2)) (assign :pc 0))))))
-           (adl->lisb '(adl
-                         :Multiply
-                         (var :x (in :x nat-set) 5)
-                         (var :y (in :y nat-set) 3)
-                         (var :p (in :p nat-set) 0)
-                         (algorithm
-                           (while (> :x 0)
-                             #_(assert (= (+ :p (* :x :y)) (* 5 3)))
-                             (if (not= 0 (mod :x 2))
-                               (assign :p (+ :p :y)))
-                             (assign :x (/ :x 2) :y (* :y 2)))
-                           #_(assert (= :p (* :x :y))))))))))
-
-#_(deftest var-test
-  (testing "equality-predicates"
-    (is (= '(machine
-              :ADL
-              (variables :pc :x :y)
-              (invariants
-                (in :pc nat-set)
-                (in :x nat-set)
-                (in :y nat-set))
-              (init
-                (assign :pc 0)
-                (assign :x 5)
-                (assign :y 3))
-              (operations))
-           (adl->lisb '(adl
-                         :ADL
-                         (var :x (in :x nat-set) 5)
-                         (var :y (in :y nat-set) 3)))))))
-
-#_(deftest default-machine-test
-  (testing "equality-predicates"
-    (is (= '(machine
-              :ADL
-              (variables :pc)
-              (invariants (in :pc nat-set))
-              (init
-                (assign :pc 0))
-              (operations))
-           (adl->lisb '(adl :ADL))))))
-
 (deftest multiply-example-test
   (testing "multiply-example"
-    (is (=
-          {:invariants [(bimplication (b= :pc 1) (b= (b+ :p (b* :x :y)) (b* 5 3)))
-                        (bimplication (b= :pc 5) (b= :p (b* :x :y)))]
-           :operations [
-                        (bop :while_enter0 [] (bprecondition (band (b= :pc 0) (b> :x 0)) (bassign :pc 1)))
-                        (bop :while_exit0 [] (bprecondition (band (b= :pc 0) (bnot (b> :x 0))) (bassign :pc 5)))
-                        (bop :assert1 [] (bprecondition (b= :pc 1) (bassign :pc 2)))
-                        (bop :ifte-then2 [] (bprecondition (band (b= :pc 2) (bnot= 0 (bmod :x 2))) (bassign :pc 3)))
-                        (bop :ifte-else2 [] (bprecondition (band (b= :pc 2) (bnot (bnot= 0 (bmod :x 2)))) (bassign :pc 4)))
-                        (bop :assign3 [] (bprecondition (b= :pc 3) (bsequential-sub (bassign :p (b+ :p :y)) (bassign :pc 4))))
-                        (bop :assign4 [] (bprecondition (b= :pc 4) (bsequential-sub (bassign :x (bdiv :x 2) :y (b* :y 2)) (bassign :pc 0))))
-                        (bop :assert5 [] (bprecondition (b= :pc 5) (bassign :pc 6)))]}
-          (algorithm
-            (while (> :x 0)
-              (assert (= (+ :p (* :x :y)) (* 5 3)))
-              (if (not= 0 (mod :x 2))
-                (assign :p (+ :p :y)))
-              (assign :x (/ :x 2) :y (* :y 2)))
-            (assert (= :p (* :x :y))))))))
+    (is (= (bmachine
+              :Multiply
+              (bvariables :pc :x :y :p)
+              (binvariants
+                (bmember? :pc bnat-set)
+                (bmember? :x bnat-set)
+                (bmember? :y bnat-set)
+                (bmember? :p bnat-set)
+                (bimplication (b= :pc 1) (b= (b+ :p (b* :x :y)) (b* 5 3)))
+                (bimplication (b= :pc 5) (b= :p (b* :x :y))))
+              (binit (bassign :pc 0) (bassign :x 5) (bassign :y 3) (bassign :p 0))
+              (boperations
+                (bop :while-enter0 [] (bprecondition (band (b= :pc 0) (b> :x 0)) (bassign :pc 1)))
+                (bop :while-exit0 [] (bprecondition (band (b= :pc 0) (bnot (b> :x 0))) (bassign :pc 5)))
+                (bop :assert1 [] (bprecondition (b= :pc 1) (bassign :pc 2)))
+                (bop :if-then2 [] (bprecondition (band (b= :pc 2) (bnot= 0 (bmod :x 2))) (bassign :pc 3)))
+                (bop :if-else2 [] (bprecondition (band (b= :pc 2) (bnot (bnot= 0 (bmod :x 2)))) (bassign :pc 4)))
+                (bop :assign3 [] (bprecondition (b= :pc 3) (bsequential-sub (bassign :p (b+ :p :y)) (bassign :pc 4))))
+                (bop :assign4 [] (bprecondition (b= :pc 4) (bsequential-sub (bassign :x (bdiv :x 2) :y (b* :y 2)) (bassign :pc 0))))
+                (bop :assert5 [] (bprecondition (b= :pc 5) (bassign :pc 6)))))
+           (adl
+             :Multiply
+             (var :x (in :x nat-set) 5)
+             (var :y (in :y nat-set) 3)
+             (var :p (in :p nat-set) 0)
+             (algorithm
+               (while (> :x 0)
+                 (assert (= (+ :p (* :x :y)) (* 5 3)))
+                 (if (not= 0 (mod :x 2))
+                   (assign :p (+ :p :y)))
+                 (assign :x (/ :x 2) :y (* :y 2)))
+               (assert (= :p (* :x :y)))))))))
+
+(deftest var-test
+  (testing "equality-predicates"
+    (is (= (bmachine
+             :ADL
+             (bvariables :pc :x :y)
+             (binvariants
+               (bmember? :pc bnat-set)
+               (bmember? :x bnat-set)
+               (bmember? :y bnat-set))
+             (binit
+               (bassign :pc 0)
+               (bassign :x 5)
+               (bassign :y 3))
+             (boperations (bop :assign0 [] (bprecondition (b= :pc 0) (bsequential-sub (bassign :x 1) (bassign :pc 1))))))
+           (adl
+             :ADL
+             (var :x (member? :x nat-set) 5)
+             (var :y (member? :y nat-set) 3)
+             (algorithm
+               (assign :x 1)))))))
+
+(deftest empty-machine-test
+  (testing "empty-machine"
+    (is (= (bmachine
+              :ADL
+              (bvariables :pc)
+              (binvariants (bmember? :pc bnat-set))
+              (binit
+                (bassign :pc 0))
+              (boperations))
+           (adl :ADL)))))
 
 (deftest do-test
   (testing "do"
     (is (= {:invariants []
             :operations [(bop :assign0 [] (bprecondition (b= :pc 0) (bsequential-sub (bassign :x 0) (bassign :pc 1))))
-                         (bop :ifte-then1 [] (bprecondition (band (b= :pc 1) (b> :x 1)) (bassign :pc 2)))
-                         (bop :ifte-else1 [] (bprecondition (band (b= :pc 1) (bnot (b> :x 1))) (bassign :pc 4)))
+                         (bop :if-then1 [] (bprecondition (band (b= :pc 1) (b> :x 1)) (bassign :pc 2)))
+                         (bop :if-else1 [] (bprecondition (band (b= :pc 1) (bnot (b> :x 1))) (bassign :pc 4)))
                          (bop :assign2 [] (bprecondition (b= :pc 2) (bsequential-sub (bassign :x 2) (bassign :pc 3))))
                          (bop :assign3 [] (bprecondition (b= :pc 3) (bsequential-sub (bassign :x 3) (bassign :pc 4))))]}
            (algorithm
@@ -102,11 +90,11 @@
   (testing "if"
     (is (= {:invariants []
             :operations [
-                         (bop :ifte-then0 [] (bprecondition (band (b= :pc 0) (b> :x 0)) (bassign :pc 1)))
-                         (bop :ifte-else0 [] (bprecondition (band (b= :pc 0) (bnot (b> :x 0))) (bassign :pc 2)))
+                         (bop :if-then0 [] (bprecondition (band (b= :pc 0) (b> :x 0)) (bassign :pc 1)))
+                         (bop :if-else0 [] (bprecondition (band (b= :pc 0) (bnot (b> :x 0))) (bassign :pc 2)))
                          (bop :assign1 [] (bprecondition (b= :pc 1) (bsequential-sub (bassign :x 1) (bassign :pc 2))))
-                         (bop :ifte-then2 [] (bprecondition (band (b= :pc 2) (b> :x 2)) (bassign :pc 3)))
-                         (bop :ifte-else2 [] (bprecondition (band (b= :pc 2) (bnot (b> :x 2))) (bassign :pc 4)))
+                         (bop :if-then2 [] (bprecondition (band (b= :pc 2) (b> :x 2)) (bassign :pc 3)))
+                         (bop :if-else2 [] (bprecondition (band (b= :pc 2) (bnot (b> :x 2))) (bassign :pc 4)))
                          (bop :assign3 [] (bprecondition (b= :pc 3) (bsequential-sub (bassign :x 3) (bassign :pc 5))))
                          (bop :assign4 [] (bprecondition (b= :pc 4) (bsequential-sub (bassign :y 4) (bassign :pc 5))))]}
            (algorithm
@@ -120,11 +108,11 @@
   (testing "while"
     (is (= {:invariants []
             :operations [
-                         (bop :while_enter0 [] (bprecondition (band (b= :pc 0) (b> :x 0)) (bassign :pc 1)))
-                         (bop :while_exit0 [] (bprecondition (band (b= :pc 0) (bnot (b> :x 0))) (bassign :pc 2)))
+                         (bop :while-enter0 [] (bprecondition (band (b= :pc 0) (b> :x 0)) (bassign :pc 1)))
+                         (bop :while-exit0 [] (bprecondition (band (b= :pc 0) (bnot (b> :x 0))) (bassign :pc 2)))
                          (bop :assign1 [] (bprecondition (b= :pc 1) (bsequential-sub (bassign :x 1) (bassign :pc 0))))
-                         (bop :while_enter2 [] (bprecondition (band (b= :pc 2) (b> :x 2)) (bassign :pc 3)))
-                         (bop :while_exit2 [] (bprecondition (band (b= :pc 2) (bnot (b> :x 2))) (bassign :pc 5)))
+                         (bop :while-enter2 [] (bprecondition (band (b= :pc 2) (b> :x 2)) (bassign :pc 3)))
+                         (bop :while-exit2 [] (bprecondition (band (b= :pc 2) (bnot (b> :x 2))) (bassign :pc 5)))
                          (bop :assign3 [] (bprecondition (b= :pc 3) (bsequential-sub (bassign :x 3) (bassign :pc 4))))
                          (bop :assign4 [] (bprecondition (b= :pc 4) (bsequential-sub (bassign :y 4) (bassign :pc 2))))]}
            (algorithm
