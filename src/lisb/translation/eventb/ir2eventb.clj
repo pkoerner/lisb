e660258ac24138e2e6a55720c9e7ec113f5c51cf
Peter Jakob Julius Armbrüster <julius.armbruester@hhu.de>
2023-01-30 23:50:14 +0100

    split expression and predicate string creation

--- src/lisb/translation/eventb/ir2eventb.clj
+++ src/lisb/translation/eventb/ir2eventb.clj
@@ -24,151 +24,142 @@
     Invariant)
    de.prob.model.representation.ModelElementList))
 
-(defmulti ir-node->str (fn [ir-node] (or (:tag ir-node) (class ir-node))))
+(defmulti ir-expr->str (fn [ir] (or (:tag ir) (class ir))))
+(defmulti ir-pred->str (fn [ir] (or (:tag ir) (class ir))))
 
-(defn chain->str [op elems]
-  (str/join "&"
-            (map (fn [a b] (str (ir-node->str a) op (ir-node->str b)))
-                 (butlast elems) (rest elems))))
+(defn chain-expr [op ir]
+  (str "(" (str/join op (map ir-expr->str ir)) ")"))
 
-(defn apply->str [op args]
-  (str "(" (str/join op (map ir-node->str args)) ")"))
+(defn chain-pred [op ir]
+  (str "(" (str/join op (map ir-pred->str ir)) ")"))
 
 ;; To Strings
 
-(defmethod ir-node->str clojure.lang.Keyword [ir-node] (name ir-node))
-
-(defmethod ir-node->str java.lang.Long [ir-node] (str ir-node))
+(defmethod ir-expr->str clojure.lang.Keyword [ir] (name ir))
+(defmethod ir-expr->str java.lang.Long [ir] (str ir))
 
 ;; Boolean
 
-(defmethod ir-node->str java.lang.Boolean [ir-node] (str/upper-case (str ir-node)))
-
-(defmethod ir-node->str :bool-set [ir-node] "BOOL")
-
-(defmethod ir-node->str :pred->bool [ir-node]
-  (str "bool(" (ir-node->str (:pred ir-node)) ")"))
+(defmethod ir-expr->str java.lang.Boolean [ir] (str/upper-case (str ir)))
+(defmethod ir-expr->str :bool-set [_] "BOOL")
+(defmethod ir-expr->str :pred->bool [ir]
+  (str "bool(" (ir-expr->str (:pred ir)) ")"))
 
 ;; Numbers
 
-(defmethod ir-node->str :add [ir-node] (apply->str " + " (:nums ir-node)))
-(defmethod ir-node->str :sub [ir-node] (apply->str " - " (:nums ir-node)))
-(defmethod ir-node->str :cart-or-mul [ir-node] (apply->str "*" (:nums-or-sets ir-node)))
-(defmethod ir-node->str :div [ir-node] (apply->str " / " (:nums ir-node)))
-(defmethod ir-node->str :mod [ir-node] (apply->str " mod " (:nums ir-node)))
-;;(defmethod ir-node->str :pow [ir-node] (apply->str " ** " (:nums ir-node)))
-(defmethod ir-node->str :less [ir-node] (apply->str " < " (:nums ir-node)))
-(defmethod ir-node->str :less-equals [ir-node] (apply->str " <= " (:nums ir-node)))
-(defmethod ir-node->str :greater [ir-node] (apply->str " > " (:nums ir-node)))
-(defmethod ir-node->str :greater-equals [ir-node] (apply->str " >= " (:nums ir-node)))
+(defmethod ir-expr->str :add [ir] (chain-expr " + " (:nums ir)))
+(defmethod ir-expr->str :sub [ir] (chain-expr " - " (:nums ir)))
+(defmethod ir-expr->str :cart-or-mul [ir] (chain-expr "*" (:nums-or-sets ir)))
+(defmethod ir-expr->str :div [ir] (chain-expr " / " (:nums ir)))
+(defmethod ir-expr->str :mod [ir] (chain-expr " mod " (:nums ir)))
+;;(defmethod ir-expr->str :pow [ir-expr] (chain-expr " ** " (:nums ir-expr)))
 
-(defmethod ir-node->str :integer-set [ir-node] "INT")
-(defmethod ir-node->str :int-set [ir-node] "INT")
-(defmethod ir-node->str :natural-set [ir-node] "NAT")
-(defmethod ir-node->str :nat-set [ir-node] "NAT")
-(defmethod ir-node->str :natural1-set [ir-node] "NAT1")
-(defmethod ir-node->str :nat1-set [ir-node] "NAT1")
-(defmethod ir-node->str :interval [ir-node]
-  (str (ir-node->str (:from ir-node)) ".." (ir-node->str (:to ir-node))))
-(defmethod ir-node->str :max [ir-node]
-  (str "max(" (ir-node->str (:set ir-node)) ")"))
-(defmethod ir-node->str :min [ir-node]
-  (str "min(" (ir-node->str (:set ir-node)) ")"))
-(defmethod ir-node->str :successor [ir-node]
-  (str (ir-node->str (:num ir-node)) "+1"))
-(defmethod ir-node->str :predecessor [ir-node]
-  (str (ir-node->str (:num ir-node)) "-1"))
+(defmethod ir-expr->str :integer-set [_] "INT")
+(defmethod ir-expr->str :int-set [_] "INT")
+(defmethod ir-expr->str :natural-set [_] "NAT")
+(defmethod ir-expr->str :nat-set [_] "NAT")
+(defmethod ir-expr->str :natural1-set [_] "NAT1")
+(defmethod ir-expr->str :nat1-set [_] "NAT1")
+(defmethod ir-expr->str :interval [ir]
+  (str (ir-expr->str (:from ir)) ".." (ir-expr->str (:to ir))))
+(defmethod ir-expr->str :max [ir]
+  (str "max(" (ir-expr->str (:set ir)) ")"))
+(defmethod ir-expr->str :min [ir]
+  (str "min(" (ir-expr->str (:set ir)) ")"))
+(defmethod ir-expr->str :successor [ir]
+  (str (ir-expr->str (:num ir)) "+1"))
+(defmethod ir-expr->str :predecessor [ir]
+  (str (ir-expr->str (:num ir)) "-1"))
 
 ;; Logical predicates
 
-(defmethod ir-node->str :and [ir-node] (apply->str " & " (:preds ir-node)))
-(defmethod ir-node->str :or [ir-node] (apply->str " or " (:preds ir-node)))
-(defmethod ir-node->str :implication [ir-node] (apply->str " => " (:preds ir-node)))
-(defmethod ir-node->str :equivalence [ir-node] (apply->str " <=> " (:preds ir-node)))
-(defmethod ir-node->str :not [ir-node]
-  (str "not(" (ir-node->str (:pred ir-node) ")")))
-(defmethod ir-node->str :for-all [ir-node]
-  (str "!" (str/join "," (map name (:ids ir-node))) "." (ir-node->str (:implementation ir-node))))
-(defmethod ir-node->str :exists [ir-node]
-  (str "#" (str/join "," (map name (:ids ir-node)) (ir-node->str (:pred ir-node)))))
+(defmethod ir-pred->str :less [ir] (chain-expr " < " (:nums ir)))
+(defmethod ir-pred->str :less-equals [ir] (chain-expr " <= " (:nums ir)))
+(defmethod ir-pred->str :greater [ir] (chain-expr " > " (:nums ir)))
+(defmethod ir-pred->str :greater-equals [ir] (chain-expr " >= " (:nums ir)))
+
+(defmethod ir-pred->str :and [ir] (chain-pred " & " (:preds ir)))
+(defmethod ir-pred->str :or [ir] (chain-pred " or " (:preds ir)))
+(defmethod ir-pred->str :implication [ir] (chain-pred " => " (:preds ir)))
+(defmethod ir-pred->str :equivalence [ir] (chain-pred " <=> " (:preds ir)))
+(defmethod ir-pred->str :not [ir]
+  (str "not(" (ir-pred->str (:pred ir) ")")))
+(defmethod ir-pred->str :for-all [ir]
+  (str "!" (str/join "," (map name (:ids ir))) "." (ir-pred->str (:implementation ir))))
+(defmethod ir-pred->str :exists [ir]
+  (str "#" (str/join "," (map name (:ids ir)) (ir-pred->str (:pred ir)))))
 
 ;; Equality
 
-(defmethod ir-node->str :equals [ir-node]
-  (str (ir-node->str (:left ir-node)) "=" (ir-node->str (:right ir-node))))
+(defmethod ir-pred->str :equals [ir]
+  (str (ir-expr->str (:left ir)) "=" (ir-expr->str (:right ir))))
 
-(defmethod ir-node->str :not-equals [ir-node]
-  (str (ir-node->str (:left ir-node)) "/=" (ir-node->str (:right ir-node))))
+(defmethod ir-pred->str :not-equals [ir]
+  (str (ir-expr->str (:left ir)) "/=" (ir-expr->str (:right ir))))
 
 ;; Sets
 
-(defmethod ir-node->str clojure.lang.PersistentHashSet [ir-node]
-  (str "{" (str/join "," (map ir-node->str ir-node)) "}"))
+(defmethod ir-expr->str clojure.lang.PersistentHashSet [ir]
+  (str "{" (str/join "," (map ir-expr->str ir)) "}"))
 
-(defmethod ir-node->str :comprehension-set [ir-node]
+(defmethod ir-expr->str :comprehension-set [ir]
   (str "{"
-       (str/join "," (map name (:ids ir-node))) "|"
-       (ir-node->str (:pred ir-node)) "}" ))
+       (str/join "," (map name (:ids ir))) "|"
+       (ir-expr->str (:pred ir)) "}" ))
 
-(defmethod ir-node->str :power-set [ir-node]
-  (str "POW(" (ir-node->str (:set ir-node)) ")"))
+(defmethod ir-expr->str :power-set [ir]
+  (str "POW(" (ir-expr->str (:set ir)) ")"))
 
-(defmethod ir-node->str :power1-set [ir-node]
-  (str "POW1(" (ir-node->str (:set ir-node)) ")"))
+(defmethod ir-expr->str :power1-set [ir]
+  (str "POW1(" (ir-expr->str (:set ir)) ")"))
 
-(defmethod ir-node->str :card [ir-node]
-  (str "card(" (ir-node->str (:set ir-node)) ")"))
+(defmethod ir-expr->str :card [ir]
+  (str "card(" (ir-expr->str (:set ir)) ")"))
 
-(defmethod ir-node->str :unite-sets [ir-node]
-  (str "union(" (ir-node->str (:set ir-node)) ")"))
+(defmethod ir-expr->str :unite-sets [ir]
+  (str "union(" (ir-expr->str (:set ir)) ")"))
 
-(defmethod ir-node->str :unite-sets [ir-node]
-  (str "inter(" (ir-node->str (:set ir-node)) ")"))
+(defmethod ir-expr->str :unite-sets [ir]
+  (str "inter(" (ir-expr->str (:set ir)) ")"))
 
-(defmethod ir-node->str :cart-or-mul [ir-node]
-  (apply->str "*" (:nums-or-sets ir-node)))
+(defmethod ir-expr->str :cart-or-mul [ir]
+  (chain-expr "*" (:nums-or-sets ir)))
 
-(defmethod ir-node->str :union [ir-node]
-  (apply->str "\\/" (:sets ir-node)))
+(defmethod ir-expr->str :union [ir]
+  (chain-expr "\\/" (:sets ir)))
 
-(defmethod ir-node->str :intersection [ir-node]
-  (apply->str "/\\" (:sets ir-node)))
+(defmethod ir-expr->str :intersection [ir]
+  (chain-expr "/\\" (:sets ir)))
 
-(defmethod ir-node->str :member [ir-node]
-  (str (ir-node->str (:elem ir-node)) ":" (ir-node->str (:set ir-node))))
+(defmethod ir-pred->str :member [ir]
+  (str (ir-expr->str (:elem ir)) ":" (ir-expr->str (:set ir))))
 
-(defmethod ir-node->str :subset [ir-node]
-  (chain->str "<:" (:sets ir-node)))
+(defn chain-expr-explicit [op elems]
+  (str/join " & "
+            (map (fn [a b] (str (ir-expr->str a) op (ir-expr->str b)))
+                 (butlast elems) (rest elems))))
 
-(defmethod ir-node->str :strict-subset [ir-node]
-  (chain->str "<<:" (:sets ir-node)))
+(defmethod ir-pred->str :subset [ir]
+  (chain-expr-explicit "<:" (:sets ir)))
+
+(defmethod ir-pred->str :strict-subset [ir]
+  (chain-expr-explicit "<<:" (:sets ir)))
 
 ;; Relations
 
 ;; Functions
 
  ;; TODO: allow multiple args
-(defmethod ir-node->str :fn-call [ir-node]
-  (str (name (:f ir-node)) "(" (ir-node->str (first (:args ir-node))) ")"))
-
-;; Substitutions
-
-(defn id-vals->ids [id-vals]
-  (map (comp ir-node->str first) (partition 2 id-vals)))
-
-(defn id-vals->vals [id-vals]
-  (map (comp ir-node->str second) (partition 2 id-vals)))
-
-;; TODO: If more then 1 id is present non of them can be a :fn-call.
-(defmethod ir-node->str :assignment [ir-node]
-  (s/assert (s/keys :req-un [::id-vals]) ir-node)
-  (let [id-vals (:id-vals ir-node)
-        ids (id-vals->ids id-vals)
-        vals (id-vals->vals id-vals)]
-    (str (str/join "," ids) " := " (str/join "," vals))
-    ))
+(defmethod ir-expr->str :fn-call [ir]
+  (str (name (:f ir)) "(" (name (first (:args ir))) ")"))
 
 ;; Construct ProB Model
+
+(defn id-vals->ids [id-vals]
+  (map (comp ir-expr->str first) (partition 2 id-vals)))
+
+(defn id-vals->vals [id-vals]
+  (map (comp ir-expr->str second) (partition 2 id-vals)))
 
 (defn action [name code] (EventBAction. name code #{}))
 
@@ -183,20 +174,31 @@
 
 (defn variable [name] (EventBVariable. name nil))
 
-(defn has-tag [tag] (fn [ir-node] (= tag (:tag ir-node))))
+(defn has-tag [tag] (fn [ir] (= tag (:tag ir))))
 
-(defn extract-actions [ir-node]
-  (case (:tag ir-node)
-    :assignment [(action "act0" (ir-node->str ir-node))]
-    :parallel-sub (map-indexed
-                   (fn [i n] (action (str "act" i) (ir-node->str n)))
-                   (:subs ir-node))
-    :precondition (extract-actions (first (:subs ir-node)))
-    []))
+;; TODO: If more then 1 id is present non of them can be a :fn-call.
+(defn extract-actions-code
+  [ir]
+  (case (:tag ir)
+    :assignment
+    (let [ids (id-vals->ids (:id-vals ir))
+          vals (id-vals->vals (:id-vals ir))
+          code (str (str/join "," ids) " := " (str/join "," vals))]
+      [code])
 
-(defn extract-guards [ir-node]
-  (case (:tag ir-node)
-    :precondition [(guard "grd0" (ir-node->str (:pred ir-node)))]
+    :parallel-sub
+    (mapcat extract-actions-code (:subs ir))
+
+    :precondition
+    (extract-actions-code (first (:subs ir)))))
+
+(defn extract-actions [ir]
+  (map-indexed (fn [i code] (action (str "act" i) code))
+               (extract-actions-code ir)))
+
+(defn extract-guards [ir]
+  (case (:tag ir)
+    :precondition [(guard "grd0" (ir-pred->str (:pred ir)))]
     []))
 
 (defn op->event [op]
@@ -216,12 +218,13 @@
 (defn extract-init [clauses]
   (->> clauses
        (find-first :init)
-       (map-indexed (fn [i n] (action (str "init" i) (ir-node->str n))))
+       (mapcat extract-actions-code)
+       (map-indexed (fn [i code] (action (str "init" i) code)))
        (event "INITIALISATION")))
 
 (defn extract-invariants [clauses]
   (map-indexed
-   (fn [i pred] (invariant (str "inv" i) (ir-node->str pred)))
+   (fn [i pred] (invariant (str "inv" i) (ir-pred->str pred)))
    (find-first :invariants clauses)))
 
 (defn extract-variables [clauses]
@@ -240,10 +243,10 @@
 (defn set-variables [m vars]
   (.set m Variable (ModelElementList. vars)))
 
-(defn ir->eventb-machine [ir-node]
-  (assert (= :machine (:tag ir-node)))
-  (let [m-name (-> ir-node :name name (str/replace #"-" "_"))
-        clauses (:machine-clauses ir-node)]
+(defn ir->eventb-machine [ir]
+  (assert (= :machine (:tag ir)))
+  (let [m-name (-> ir :name name (str/replace #"-" "_")) ;; rodin does not allow "-" in machine names
+        clauses (:machine-clauses ir)]
     (-> (EventBMachine. m-name)
         (set-invariants (extract-invariants clauses))
         (set-events (conj (extract-events clauses) (extract-init clauses)))
@@ -256,17 +259,24 @@
   (.writeToRodin (ModelToXML.) model model-name path))
 
 (comment
-  (ir-node->str (util/b (strict-subset? #{1} #{1,2} #{1,2,3})))
-  (ir-node->str (util/b (assign :x (+ :E 1) :y :W)))
-  (ir-node->str (util/b (assign (fn-call :f :x) (+ :E 1) :y :W)))
-  (ir-node->str (util/b (and (< :x 10) (> :y 0))))
+  (ir-pred->str (util/b (strict-subset? #{1} #{1,2} #{1,2,3})))
+  (ir-pred->str (util/b (and (< :x 10) (> :y 0))))
+
+  (extract-actions (util/b (assign :x (+ :E 1) :y :W)))
+  (extract-actions (util/b (assign (fn-call :f :x) (+ :E 1) :y :W)))
 
   (extract-actions (util/b (assign :x (+ :E 1) :y :W)))
   (extract-actions (util/b (||
                             (assign :y :W)
                             (assign :x (+ :E 1) :y :W))))
 
-  (extract-actions (util/b  (+ :E 1)))
+  ;; concat all parallel assignments
+  (extract-actions-code (util/b (||
+                            (assign :y :W)
+                            (||
+                             (assign :z :Z)
+                             (assign :w (+ :E 13)))
+                            (assign :x (+ :E 1) :y :W))))
 
   (extract-guards (util/b (pre (and (< :x 10) (> :y 0)) (assign :x :y))))
 
