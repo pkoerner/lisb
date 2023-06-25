6d21a6b1323b917261d2504d6cbb4fe52eb6e2ff
Peter Jakob Julius Armbrüster <julius.armbruester@hhu.de>
2023-06-25 13:14:34 +0200

    use utils

--- test/lisb/translation/eventb/cycle_test.clj
+++ test/lisb/translation/eventb/cycle_test.clj
@@ -1,7 +1,5 @@
 (ns lisb.translation.eventb.cycle-test
-  (:require [lisb.translation.eventb.eventb2lisb :refer [prob->lisb]]
-            [lisb.translation.eventb.ir2eventb :refer [ir->prob]]
-            [lisb.translation.eventb.dsl :refer [eventb lisb->ir]]
+  (:require [lisb.translation.eventb.util :refer [eventb lisb->ir ir->prob prob->lisb]]
             [clojure.test :refer [deftest are]]))
 
 (deftest machine-context-test
@@ -10,26 +8,26 @@
     (eventb (context :Empty_ctx)))
   )
 
-(deftest machine-test 
+(deftest machine-test
   (are [ir] (= ir (-> ir ir->prob prob->lisb lisb->ir))
     (eventb (machine :foo
                      (variables :x :y :z)
                      (init
                       (assign :x 1 :y 2)
                       (assign :z :nat))
-                     (events 
+                     (events
                       (event :magic (any :t) (when (> :x 0)) (then (assign :x :t))))))))
 
-(deftest event-test 
+(deftest event-test
   (are [ir] (= ir (-> ir ir->prob prob->lisb lisb->ir))
     (eventb (event :empty))
-    (eventb (event :event1 (then (assign :x 1 :y 2)))) 
+    (eventb (event :event1 (then (assign :x 1 :y 2))))
     (eventb (event :event1 (when (> :y 2))))
     (eventb (event :event1 (any :t)))
     ))
 
 (comment
-  (-> (eventb (event :event1 (any :t))) 
+  (-> (eventb (event :event1 (any :t)))
       ir->prob
       prob->lisb
       )
