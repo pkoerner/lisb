dd71ecc1a317629f6282d45a7938a4f9cfdccd8c
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2023-08-17 15:04:03 +0200

    Problem: rules DSL WIP had compile errors and lacked translation to IR
    Solution: added some transformation into a partial machine

--- src/lisb/dsls/rules.clj
+++ src/lisb/dsls/rules.clj
@@ -1,4 +1,5 @@
-(ns lisb.dsls.rules)
+(ns lisb.dsls.rules
+  (:require [lisb.translation.lisb2ir :refer :all]))
 
 ;; https://prob.hhu.de/w/index.php?title=Rules-DSL
 
@@ -12,29 +13,32 @@
    :body body})
 
 (defn rule-forall [ids where expect error-type counterexample]
-  {:identifiers ids
+  {:tag :rule-forall
+   :identifiers ids
    :where where ; conditions on identifiers
    :expect expect ; conditions that must be fulfilled for this rule
-   :error-type ; number in range of error-types
+   :error-type error-type ; number in range of error-types
    :counterexample counterexample ; of type INTEGER <-> STRING ; STRING_FORMAT("errorMessage ~w", identifier from list)
    })
 
 (defn rule-fail [ids whenn error-type counterexample]
-  {:identifiers ids
+  {:tag :rule-fail
+   :identifiers ids
    :when whenn ; conditions on identifiers for a failing rule
-   :error-type ; number in range of error-types
+   :error-type error-type ; number in range of error-types
    :counterexample counterexample ; of type INTEGER <-> STRING ; STRING_FORMAT("errorMessage ~w", identifier from list)
    })
 
 
-(defn computation [rule-dependencies computation-dependency activation replaces body]
-  {:rule-dependencies rules-dependencies
+(defn computation [namey rule-dependencies computation-dependencies activation replaces body]
+  {:name namey
+   :rule-dependencies rule-dependencies
    :computation-dependencies computation-dependencies
    :activation activation
    :replaces replaces ;; identifier of exactly one computation
    :body body })
 
-(defn computation-body [define typey dummy-value value]
+(defn computation-body [variable-name typey dummy-value value]
   {:define variable-name
    :type typey
    :dummy-value dummy-value ;; value before computation happens
@@ -43,13 +47,108 @@
 
 (defn function [output namey params precondition postcondition body output-assignment]
   {:output output ; variable name
-   :name :namey ; function name
+   :name namey ; function name
    :params params
    :precondition precondition ; predicate parameters must fulfill
    :postcondition postcondition ; output value(s) must satisfy postcondition
    :body body ; b statements
    :output-assignment output-assignment })
 
+(defmulti body->substs (fn [body ctx] (:tag body)))
+(defmethod body->substs :rule-forall [m rule]
+  (bif-sub (bnot (bfor-all (get m :identifiers)
+                           (get m :where)
+                           (get m :expect)))
+           (bsequential-sub
+             (bassign (keyword (str (name (:name rule)) "_Counterexamples"))
+                      (bunion (keyword (str (name (:name rule)) "_Counterexamples"))
+                              (bmaplet (get m :error-type) (get m :counterexample))))
+             (bassign (:name rule) "FAIL"
+                    :$RESULT "FAIL"
+                    :$COUNTEREXAMPLES (keyword (str (name (:name rule)) "_Counterexamples"))))))
+(defmethod body->substs :rule-fail [m rule]
+  (bif-sub (if (get m :identifiers)
+             (bexists (get m :identifiers) (get m :when))
+             (get m :when))
+           (bsequential-sub
+             (bassign (keyword (str (name (:name rule)) "_Counterexamples"))
+                      (bunion (keyword (str (name (:name rule)) "_Counterexamples"))
+                              (bmaplet (get m :error-type) (get m :counterexample))))
+             (bassign (:name rule) "FAIL"
+                    :$RESULT "FAIL"
+                    :$COUNTEREXAMPLES (keyword (str (name (:name rule)) "_Counterexamples"))))))
+
+(defn rule->machineparts [rule]
+  {:variables [(:name rule) ;; string "SUCCESS", "NOT_CHECKED", "FAIL"
+               (keyword (str (name (:name rule)) "_Counterexamples")) ;; {} ; mapping error code to string message?
+               ]
+   :operations (bop [:$RESULT :$COUNTEREXAMPLES] ; return values
+                    (:name rule)
+                    [] ; no params
+                    (bselect (apply band (concat [(:activation rule)]
+                                                 (map (partial b= "SUCCESS") (get-in rule [:dependencies :rules]))
+                                                 (map (partial b= "EXECUTED") (get-in rule [:dependencies :computation-dependencies]))))
+                             (bsequential-sub (apply bsequential-sub (mapcat body->substs (get rule :body)))
+                                              (bif-sub (bnot= (:name rule) "FAIL")
+                                                       (bassign (:name rule) "SUCCESS")))))})
+
+(defn computation-body->mch-parts [comp-part]
+  {:variables [(get comp-part :define)]
+   :properties (bmember? (get comp-part :define) (get comp-part :type))
+   :initialisation (bassign (get comp-part :define) (get comp-part :dummy-value))
+   :assignments (bassign (get comp-part :define) (get comp-part :value))})
+
+(defn merge-keys-with 
+  ([merge-map m] m)
+  ([merge-map m1 m2]
+   (reduce (fn [m [k v]]
+             (if (contains? m k)
+               (update m k (get merge-map k) v)
+               (assoc m k v)))
+           m1
+           m2)) 
+  ([merge-map m1 m2 m3 & ms]
+   (reduce (partial merge-keys-with merge-map) m1 (cons m2 (cons m3 ms)))))
+
+
+(defn computation->machineparts [computation]
+  (let [base (apply merge-keys-with 
+                    {:variables concat
+                     :properties band
+                     :initialisation bparallel-sub
+                     :assignments bsequential-sub }
+                    (map computation-body->mch-parts (get computation :body)))]
+    (-> base
+        (dissoc :assignments)
+        (assoc :operations [(bop (get computation :name)
+                                 []
+                                 (bselect (apply band (:activation computation)
+                                                 (concat 
+                                                   (map (partial b= "SUCCESS") (get computation :rule-dependencies))
+                                                   (map (partial b= "EXECUTED") (get computation :computation-dependencies))))
+                                          (get base :assignments)))]))))
+
+
+
+(clojure.pprint/pprint
+  (computation->machineparts (computation :COMP_Numbers
+             []
+             []
+             true
+             []
+             [(computation-body :UpperBound
+                               binteger-set
+                               -1
+                               100)
+              (computation-body :EvenNumbers
+                                (bpow binteger-set)
+                                {} ;; not in original machine
+                                (bcomprehension-set [:x] (band :x (binterval 0 :Upperbound)
+                                                               (b= (bmod :x 2) 0))))
+              (computation-body :NumberOfEvenNumbers
+                                binteger-set
+                                -1
+                                (bcard :EvenNumbers))])))
 
 ;; more predicates:
 
