59c4dfcb064629865b69f64c7568aa84be929f10
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2024-04-14 21:08:57 +0200

    Problem: state objects don't feel clojure-y enough
    Solution: improve print (also support pretty prints), use proxy instead of reifying the data

--- project.clj
+++ project.clj
@@ -13,4 +13,5 @@
                  [potemkin "0.4.7"]
                  [com.rpl/specter "1.1.4"]
                  [clj-wallhack "1.0.1"]
+                 [org.flatland/ordered "1.15.11"]
                  [de.hhu.stups/prob-java "4.13.1"]])
