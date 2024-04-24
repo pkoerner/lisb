0308495399b7c8cd1e770b75012b68c176f72017
hehin101 <hehin101@hhu.de>
2024-04-24 13:18:22 +0200

    Added more generators

--- src/lisb/generation/lisb_gens.clj
+++ src/lisb/generation/lisb_gens.clj
@@ -43,11 +43,71 @@
                                                inner))]))
                      simple-num-set-gen))
 
+(def num-set-pos-pred-gen
+  (gen/one-of [(gen/fmap list*
+                         (gen/tuple (gen/elements '[subset? superset?
+                                                    strict-subset? strict-superset?])
+                                    complex-num-set-gen
+                                    complex-num-set-gen))
+               (gen/fmap list*
+                         (gen/tuple (gen/return 'member?)
+                                    complex-num-set-gen
+                                    num-expr-gen))]))
+
+(def num-set-neg-pred-gen
+  (gen/fmap list*
+            (gen/tuple (gen/return 'not)
+                       num-set-pos-pred-gen)))
+
+(def num-set-pred-gen (gen/one-of [num-set-pos-pred-gen num-set-neg-pred-gen]))
+
+
+;; logical predicates
+
+(def single-pred-gen
+  (gen/one-of [num-pred-gen num-set-pred-gen]))
+
+(def pred-gen
+  (gen/recursive-gen (fn [inner]
+                       (gen/one-of [(gen/fmap list*
+                                              (gen/tuple (gen/elements '[and or => <=>])
+                                                         inner
+                                                         inner))
+                                    (gen/fmap list*
+                                              (gen/tuple (gen/return 'not)
+                                                         inner))]))
+                     single-pred-gen))
+
+
+;; relations
+
+(def set-rel-gen
+  (gen/fmap list*
+            (gen/tuple (gen/elements '[<-> <<-> <->> <<->>])
+                       complex-num-set-gen
+                       complex-num-set-gen)))
+
+(def domain-rel-gen
+  (gen/fmap list*
+            (gen/tuple (gen/elements '[<| <<|])
+                       complex-num-set-gen
+                       set-rel-gen)))
+
+(def range-rel-gen
+  (gen/fmap list*
+            (gen/tuple (gen/elements '[|> |>>])
+                       set-rel-gen
+                       complex-num-set-gen)))
+
+(def rel-gen
+  (gen/one-of [set-rel-gen domain-rel-gen range-rel-gen]))
+
 
 ;; testing
 
 (gen/generate simple-num-set-gen)
 (gen/generate complex-num-set-gen)
+(gen/generate num-set-neg-pred-gen)
 
 (defn test-gen [g]
   (let [x (gen/generate g)]
@@ -55,7 +115,20 @@
 (test-gen num-gen)
 (test-gen num-expr-gen)
 (test-gen num-pred-gen)
+
 (test-gen simple-num-set-gen)
 (test-gen complex-num-set-gen)
-(lisb->ir '('x #{1 2} #{1 2}))
+(test-gen num-set-pos-pred-gen)
+(test-gen num-set-neg-pred-gen)
+(test-gen num-set-pred-gen)
+
+(test-gen single-pred-gen)
+(test-gen pred-gen)
+
+(test-gen set-rel-gen)
+(test-gen domain-rel-gen)
+(test-gen range-rel-gen)
+(test-gen rel-gen)
+
+(lisb->ir '(not (subset? #{2} #{1 2})))
 (lisb->ir '(pow (pow #{1 2 3})))
