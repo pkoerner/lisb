52f69e222571a67b7b28b9294c34cf945c8128a3
Philipp Körner <p.koerner@hhu.de>
2024-08-19 13:52:40 +0200

    Problem: nested tuples were not detected to be equal
    Solution: flatten tuples before comparison

--- src/lisb/translation/types.clj
+++ src/lisb/translation/types.clj
@@ -1,14 +1,26 @@
 (ns lisb.translation.types)
 
+(defprotocol Flattable
+  (make-flat [this]))
+
 (deftype Tuple 
   [xs]
+
+  Flattable
+  (make-flat [this]
+    (if (instance? Tuple (first xs))
+      (Tuple. (into (into [] (seq (make-flat (first xs)))) (map #(if (satisfies? Flattable %) (make-flat %) %) (rest xs))))
+      this))
+
   clojure.lang.Seqable
   (seq [this] (seq xs))
+
   clojure.lang.Indexed
   (nth [this i] (nth xs i))
   (nth [this i not-found] (nth xs i not-found))
+
   Object
-  (equals [this other] (and (instance? Tuple other)  (= xs (.-xs other)))) ;; TODO: are nested and flat tuples equal? I think so.
+  (equals [this other] (and (instance? Tuple other)  (= (.-xs (make-flat this)) (.-xs (make-flat other)))))
   (toString [this] (str \[ (clojure.string/join " -> " xs) \])))
 
 (defmethod clojure.core/print-method Tuple [this writer]
@@ -23,4 +35,15 @@
   (second (->Tuple [1 2]))
   (nth (seq (->Tuple [1 2 3 4])) 2)
   (nth (->Tuple [1 2 3 4]) 2)
-  (map inc (->Tuple [1 2])))
+  (map inc (->Tuple [1 2]))
+  
+  (make-flat (->Tuple [1 2 3]))
+  (make-flat (->Tuple [(->Tuple [0 1]) 2 3]))
+  (make-flat (->Tuple [(->Tuple [(->Tuple [0 1]) 2]) 3 4]))
+  (make-flat (->Tuple [(->Tuple [0 (->Tuple [1 2])]) 3 4]))
+  (= (->Tuple [1 2]) (->Tuple [1 3]))
+  (= (->Tuple [1 2 3]) (->Tuple [(->Tuple [1 2]) 3]))
+  (= (->Tuple [1 2 3]) (->Tuple [1 (->Tuple [2 3])]))
+  (= (->Tuple [0 1 2 3 4]) (->Tuple [(->Tuple [(->Tuple [0 1]) 2]) 3 4]))
+  (= (->Tuple [0 1 2 3 4]) (->Tuple [(->Tuple [0 (->Tuple [1 2])]) 3 4]))
+  )
--- test/lisb/translation/types_test.clj
+++ test/lisb/translation/types_test.clj
@@ -39,3 +39,78 @@
  (testing
   "this was deemed correct during development"
   (is (= (map inc (->Tuple [1 2])) [2 3]))))
+
+
+(deftest
+ make-flat-test-1724068141-12566
+ (testing
+  "this was deemed correct during development"
+  (is (= (make-flat (->Tuple [1 2 3])) (->Tuple [1 2 3])))))
+
+(deftest
+ make-flat-test-1724068142-12567
+ (testing
+  "this was deemed correct during development"
+  (is
+   (=
+    (make-flat (->Tuple [(->Tuple [0 1]) 2 3]))
+    (->Tuple [0 1 2 3])))))
+
+(deftest
+ make-flat-test-1724068148-12574
+ (testing
+  "this was deemed correct during development"
+  (is
+   (=
+    (make-flat (->Tuple [(->Tuple [(->Tuple [0 1]) 2]) 3 4]))
+    (->Tuple [0 1 2 3 4])))))
+
+(deftest
+ make-flat-test-1724068152-12577
+ (testing
+  "this was deemed correct during development"
+  (is
+   (=
+    (make-flat (->Tuple [(->Tuple [0 (->Tuple [1 2])]) 3 4]))
+    (->Tuple [0 (->Tuple [1 2]) 3 4])))))
+
+
+(deftest
+ =-test-1724068287-13233
+ (testing
+  "this was deemed correct during development"
+  (is (= (= (->Tuple [1 2]) (->Tuple [1 3])) false))))
+
+(deftest
+ =-test-1724068288-13236
+ (testing
+  "this was deemed correct during development"
+  (is (= (= (->Tuple [1 2 3]) (->Tuple [(->Tuple [1 2]) 3])) true))))
+
+(deftest
+ =-test-1724068288-13239
+ (testing
+  "this was deemed correct during development"
+  (is (= (= (->Tuple [1 2 3]) (->Tuple [1 (->Tuple [2 3])])) false))))
+
+(deftest
+ =-test-1724068288-13242
+ (testing
+  "this was deemed correct during development"
+  (is
+   (=
+    (=
+     (->Tuple [0 1 2 3 4])
+     (->Tuple [(->Tuple [(->Tuple [0 1]) 2]) 3 4]))
+    true))))
+
+(deftest
+ =-test-1724068288-13245
+ (testing
+  "this was deemed correct during development"
+  (is
+   (=
+    (=
+     (->Tuple [0 1 2 3 4])
+     (->Tuple [(->Tuple [0 (->Tuple [1 2])]) 3 4]))
+    false))))
