8e5abe490ec14f73f840dbd24e9d14e7aae9e556
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2025-03-12 14:54:32 +0100

    add tests

--- test/lisb/prob/animator_test.clj
+++ test/lisb/prob/animator_test.clj
@@ -7,6 +7,9 @@
 (def lift-state-space 
   (delay (state-space! (b->ast (slurp (clojure.java.io/resource "machines/b/simple/Lift.mch"))))))
 
+(def CAN-BUS 
+  (delay (state-space! (b->ast (slurp (clojure.java.io/resource "machines/b/CAN_BUS_tlc.mch"))))))
+
 (deftest root-state-test
   (testing "can retrieve root state"
     (is (root-state @lift-state-space))))
@@ -27,7 +30,12 @@
   (testing "can navigate the state space animation-like"
     (is (= 5 (:etage (:op/inc (initialised-state @lift-state-space)))))
     (is (= 3 (:etage (:op/dec (initialised-state @lift-state-space)))))
-    (is (= 7 (get-in (initialised-state @lift-state-space) [:op/inc :op/inc :op/inc :etage])))))
+    (is (= 7 (get-in (initialised-state @lift-state-space) [:op/inc :op/inc :op/inc :etage]))))
+  (testing "can navigate the state space with parameters"
+    (let [can-bus-init (get-in (root-state @CAN-BUS) [:op/$setup_constants :op/$initialise_machine])]
+      (is (= (:T3_evaluated can-bus-init false))) 
+      (is (= (:T3_evaluated true (get can-bus-init :op/Update))))
+      (is (= (:T3_evaluated true (get can-bus-init :op/Update {:pmax 0})))))))
 
 (deftest state-manipluation-test
   (testing "can update information in state"
@@ -45,3 +53,4 @@
     (let [state (to-state @lift-state-space {:etage 97})]
       (is (= {:etage 97} state))
       (is (= state {:etage 97})))))
+
