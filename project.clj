b1de5ca1328d0f55e6948f1482099cd6b9ab0015
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2025-03-28 08:06:02 +0100

    Problem: old value translator did not support big integer values
    Solution: adapt new value translator version

--- project.clj
+++ project.clj
@@ -15,5 +15,5 @@
                  [com.rpl/specter "1.1.4"]
                  [clj-wallhack "1.0.1"]
                  [de.hhu.stups/prob-java "4.13.2-SNAPSHOT"]
-                 [de.hhu.stups/value-translator "0.1.4"]
+                 [de.hhu.stups/value-translator "0.2.0-SNAPSHOT"]
                  ])
--- test/lisb/prob/retranslate_test.clj
+++ test/lisb/prob/retranslate_test.clj
@@ -2,8 +2,7 @@
   (:require [clojure.test :refer :all]
             [lisb.translation.types :refer [->Tuple]]
             [lisb.prob.retranslate :refer [retranslate]])
-  (:import (de.hhu.stups.prob.translator BAtom BBoolean BNumber BString BSet BTuple BRecord)
-           (de.hhu.stups.prob.translator.interpretations BRelation BFunction BSequence)))
+  (:import (de.hhu.stups.prob.translator BValue)))
 
 
 ;;; value types
@@ -11,86 +10,86 @@
 (deftest atom-retranslation
   (testing "retranslating an atom results in a string"
     (is (= "a"
-           (retranslate (BAtom. "a")))))
+           (retranslate (BValue/atom "a")))))
   (testing "retranslating an atom yields a native type"
     (is (= (type "a")
-           (type (retranslate (BAtom. "a")))))))
+           (type (retranslate (BValue/atom "a")))))))
 
 
 (deftest bool-retranslation
   (testing "retranslating a boolean results in the same value"
     (is (= true
-           (retranslate (BBoolean. true))))
+           (retranslate (BValue/bool true))))
     (is (= false
-           (retranslate (BBoolean. false)))))
-  (testing "retranlatinga boolean yields a native type"
+           (retranslate (BValue/bool false)))))
+  (testing "retranslating a boolean yields a native type"
     (is (= (type true)
-           (type (retranslate (BBoolean. true)))))
+           (type (retranslate (BValue/bool true)))))
     (is (= (type false)
-           (type (retranslate (BBoolean. false))))))
+           (type (retranslate (BValue/bool false))))))
   (testing "the retranslating of a boolean true is truthy"
-    (is (retranslate (BBoolean. true))))
+    (is (retranslate (BValue/bool true))))
   (testing "the retranslating of a boolean false is falsey"
-    (is (not (retranslate (BBoolean. false))))))
+    (is (not (retranslate (BValue/bool false))))))
 
 
 (deftest number-retranslation
   (testing "retranslating an integer results in the same value"
     (is (= 5
-           (retranslate (BNumber. "5")))))
+           (retranslate (BValue/number "5")))))
   (testing "retranslating an integer yields a native type"
     (is (= (type 5)
-           (type (retranslate (BNumber. "5")))))))
+           (type (retranslate (BValue/number "5")))))))
 
 
 (deftest string-retranslation
   (testing "retranslating a string results in the same value"
     (is (= "str"
-           (retranslate (BString. "str")))))
+           (retranslate (BValue/string "str")))))
   (testing "retranslating a string yields a native type"
     (is (= (type "str")
-           (type (retranslate (BString. "str")))))))
+           (type (retranslate (BValue/string "str")))))))
 
 
 ;;; collection types
 
 (deftest tuple-retranslation
-  (let [e1 (BNumber. "1")
-        e2 (BNumber. "2")]
+  (let [e1 (BValue/number "1")
+        e2 (BValue/number "2")]
     (testing "retranslating a tuple retranslates its members and results in a vector"
       (is (= (->Tuple [1 2])
-             (retranslate (BTuple. e1 e2)))))
+             (retranslate (BValue/tuple e1 e2)))))
     (testing "retranslating a tuple results in a native data type"
       (is (= (type (->Tuple [1 2]))
-             (type (retranslate (BTuple. e1 e2))))))))
+             (type (retranslate (BValue/tuple e1 e2))))))))
 
 
 (deftest record-retranslation
   (testing "retranslating an empty record results in an empty map"
     (is (= {}
-           (retranslate (BRecord. {})))))
+           (retranslate (BValue/record {})))))
   (testing "retranslating an empty record results in a native type"
     (is (= (type {})
-           (type (retranslate (BRecord. {}))))))
+           (type (retranslate (BValue/record {}))))))
   (testing "retranslating a record retranslates its members and results in a map"
-    (is (= {:a 1, :b 2} (retranslate (BRecord. {"a" (BNumber. "1"), "b" (BNumber. "2")})))))
+    (is (= {:a 1, :b 2} (retranslate (BValue/record {"a" (BValue/number "1"), "b" (BValue/number "2")})))))
   (testing "retranslating a record results in a native data type"
     (is (= (type {:a 1, :b 2})
-           (type (retranslate (BRecord. {"a" (BNumber. "1"), "b" (BNumber. "2")})))))))
+           (type (retranslate (BValue/record {"a" (BValue/number "1"), "b" (BValue/number "2")})))))))
 
 
 (deftest set-retranslation
   (testing "retranslating an empty set results in an empty set"
     (is (= #{}
-           (retranslate (BSet. #{})))))
+           (retranslate (BValue/set #{})))))
   (testing "retranslating an empty set results in a native type"
     (is (= (type #{})
-           (type (retranslate (BSet. #{}))))))
+           (type (retranslate (BValue/set #{}))))))
   (testing "retranslating a set retranslates its members and results in a set"
-    (is (= #{5} (retranslate (BSet. #{(BNumber. "5")})))))
+    (is (= #{5} (retranslate (BValue/set #{(BValue/number "5")})))))
   (testing "retranslating a set results in a native data type"
     (is (= (type #{5})
-           (type (retranslate (BSet. #{(BNumber. "5")})))))))
+           (type (retranslate (BValue/set #{(BValue/number "5")})))))))
 
 
 ;;; interpreted collection types of set
@@ -98,43 +97,52 @@
 (deftest function-retranslation
   (testing "retranslating an empty function results in an empty map"
     (is (= {}
-           (retranslate (BFunction. #{})))))
+           (retranslate (BValue/function {})))))
   (testing "retranslating an empty function results in a native type"
     (is (= (type {})
-           (type (retranslate (BFunction. #{}))))))
+           (type (retranslate (BValue/function {}))))))
   (testing "retranslating a function retranslates its members and results in a map"
     (is (= {1 4, 2 12}
-           (retranslate (BFunction. #{(BTuple. (BNumber. "1") (BNumber. "4")) (BTuple. (BNumber. "2") (BNumber. "12"))})))))
+           (retranslate (BValue/function {(BValue/number "1") (BValue/number "4"), (BValue/number "2") (BValue/number "12")})))))
   (testing "retranslating a function results in a native data type"
     (is (= (type {1 4, 2 12})
-           (type (retranslate (BFunction. #{(BTuple. (BNumber. "1") (BNumber. "4")) (BTuple. (BNumber. "2") (BNumber. "12"))})))))))
+           (type (retranslate (BValue/function {(BValue/number "1") (BValue/number "4"), (BValue/number "2") (BValue/number "12")})))))))
 
 
 (deftest relation-retranslation
   (testing "retranslating an empty relation results in an empty map"
     (is (= {}
-           (retranslate (BRelation. #{})))))
+           (retranslate (BValue/relationFromTuples #{})))))
   (testing "retranslating an empty relation results in a native type"
     (is (= (type {})
-           (type (retranslate (BRelation. #{}))))))
+           (type (retranslate (BValue/relationFromTuples #{}))))))
   (testing "retranslating a relation retranslates its members and results in a map with vectors as values"
     (is (= {1 [4 5], 2 [12]}
-           (retranslate (BRelation. #{(BTuple. (BNumber. "1") (BNumber. "4")) (BTuple. (BNumber. "1") (BNumber. "5")) (BTuple. (BNumber. "2") (BNumber. "12"))})))))
+           (retranslate (BValue/relationFromTuples #{(BValue/tuple (BValue/number "1") (BValue/number "4")) (BValue/tuple (BValue/number "1") (BValue/number "5")) (BValue/tuple (BValue/number "2") (BValue/number "12"))})))))
   (testing "retranslating a relation results in a native data type"
     (is (= (type {1 [4 5], 2 [12]})
-           (type (retranslate (BRelation. #{(BTuple. (BNumber. "1") (BNumber. "4")) (BTuple. (BNumber. "1") (BNumber. "5")) (BTuple. (BNumber. "2") (BNumber. "12"))})))))))
+           (type (retranslate (BValue/relationFromTuples #{(BValue/tuple (BValue/number "1") (BValue/number "4")) (BValue/tuple (BValue/number "1") (BValue/number "5")) (BValue/tuple (BValue/number "2") (BValue/number "12"))})))))))
 
 
 (deftest sequence-retranslation
   (testing "retranslating an empty sequence results in an empty vector"
     (is (= []
-           (retranslate (BSequence. #{})))))
+           (retranslate (BValue/sequence [])))))
   (testing "retranslating an empty sequence results in a native type"
     (is (= (type [])
-           (type (retranslate (BSequence. #{}))))))
+           (type (retranslate (BValue/sequence []))))))
   (testing "retranslating a set retranslates its members and results in a vector"
     (is (= [4 12]
-           (retranslate (BSequence. #{(BTuple. (BNumber. "1") (BNumber. "4")) (BTuple. (BNumber. "2") (BNumber. "12"))})))))
+           (retranslate (BValue/sequenceFromTuples #{(BValue/tuple (BValue/number "1") (BValue/number "4")) (BValue/tuple (BValue/number "2") (BValue/number "12"))})))))
   (testing "retranslating a sequence results in a native data type"
     (is (= (type [4 12])
-           (type (retranslate (BSequence. #{(BTuple. (BNumber. "1") (BNumber. "4")) (BTuple. (BNumber. "2") (BNumber. "12"))})))))))
+           (type (retranslate (BValue/sequenceFromTuples #{(BValue/tuple (BValue/number "1") (BValue/number "4")) (BValue/tuple (BValue/number "2") (BValue/number "12"))})))))))
+
+(deftest small-big-number-retranslation
+  (testing "small and big numbers are retranslated properly"
+    (is (= 1 
+          (retranslate (BValue/number "1"))))
+    (is (= 1 
+          (retranslate (BValue/number 1))))
+    (is (= 123745624398705623487
+          (retranslate (BValue/number "123745624398705623487"))))))
