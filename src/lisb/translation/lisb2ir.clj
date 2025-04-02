4f19d608bfe83f608fcbd354490a8c092c4c32ad
iTitus <iTitus@users.noreply.github.com>
2025-04-02 13:09:07 +0200

    revert previous commit  regarding sets and add comment explaining why

--- src/lisb/translation/lisb2ir.clj
+++ src/lisb/translation/lisb2ir.clj
@@ -1681,6 +1681,7 @@
     (and (seq? lisb) (symbol? (first lisb)) (= "operations" (name (first lisb)))) (process-op-definitions lisb)
     (and (seq? lisb) (symbol? (first lisb)) (= "<--" (name (first lisb)))) (process-assign-returns lisb)
     (and (seq? lisb) (symbol? (first lisb)) (= "if" (name (first lisb)))) (pre-process-lisb (list* lisb.translation.lisb2ir/bif (rest lisb)))
+    (set? lisb) `(hash-set ~@(map pre-process-lisb lisb)) ; make sure we do not generate code that crashes when there are duplicate elements
     (seqable? lisb) (walk pre-process-lisb identity lisb)
     :else lisb))
 
